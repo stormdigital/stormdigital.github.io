66974c3d07b01591695cbe3b31438753f33622fa
Wessel <wessel.hooghiemstra@accenture.com>
2023-10-06 12:11:48 +0200

    canvas

---
+++ preview/canvas-video-tests/js/localMain.js
@@ -0,0 +1,12 @@
+import html from "../index.html";
+import style from '!!raw-loader!../../dist/style.css';
+import main from './main.js'
+import "core-js/stable";
+import regenerator from "regenerator-runtime/runtime";
+
+// import style from '!!raw-loader!../style.css';
+
+main.init();
+
+
+
