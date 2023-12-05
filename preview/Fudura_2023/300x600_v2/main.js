7b77f465e7b8a421feaa3e8fec15bec9735c7498
Dave Tervoort <dave.tervoort@accenture.com>
2023-12-05 17:58:42 +0100

    fudura

--- preview/Fudura_2023/300x600_v2/main.js
+++ preview/Fudura_2023/300x600_v2/main.js
@@ -29,19 +29,18 @@ function init() {
 
 function getAnimation(){
 
-    var split = new SplitText("#text1", {type: "words"});
+    var split = new SplitText("#text1", {type: "lines"});
 
     var arrowTL = gsap.timeline({paused:true});
-    // arrowTL.fromTo("#bgWrapper", 0.4, {WebkitMaskPositionX:"-240%", MaskPositionX:"-240%"}, {WebkitMaskPositionX:"170%", MaskPositionX:"170%", ease:Power0.easeNone});
-    
+    arrowTL.fromTo("#bgWrapper", 0.4, {WebkitMaskPositionX:"-240%", MaskPositionX:"-240%"}, {WebkitMaskPositionX:"270%", MaskPositionX:"270%", ease:Power0.easeNone});
+    arrowTL.to("#bgWrapper", 0.00000001, {WebkitMaskPositionX:"-240%", MaskPositionX:"-240%"});
     arrowTL.to("#bgWrapper", 0.2, {WebkitMaskPositionX:"-70%", MaskPositionX:"-70%", ease:Sine.easeOut});
 
     masterTL = new TimelineLite({});
-    masterTL.to("#bgWrapper", 0.00000001, {WebkitMaskPositionX:"-240%", MaskPositionX:"-240%"});
     masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut}, 0)
     masterTL.add("start");
-    masterTL.from(["#bg", "#bg2"], 2,{x:-70, ease:Sine.easeOut}, "start");
-    masterTL.to(arrowTL, 2,{progress: 1, ease:Sine.easeOut}, "start");
+    masterTL.from(["#bg", "#bg2"], 3,{x:-70, ease:Sine.easeOut}, "start");
+    masterTL.to(arrowTL, 3,{progress: 1, ease:Sine.easeOut}, "start");
     masterTL.from("#logo", 0.5,{opacity:0, ease:Sine.easeOut}, "-=1");
     masterTL.from("#logoText", 0.5,{opacity:0, y:-20, ease:Sine.easeOut}, "-=0.3");
     masterTL.from(split.lines, 0.6, {x:-15, ease:Sine.easeOut, stagger: 0.15}, "+=0.5");
