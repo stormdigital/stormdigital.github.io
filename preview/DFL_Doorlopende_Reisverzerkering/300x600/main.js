8617b1b343f897e4e9d4ae2d839fa0716a4acfee
Dave Tervoort <dave.tervoort@accenture.com>
2024-01-15 12:09:21 +0100

    feedback DFL

--- preview/DFL_Doorlopende_Reisverzerkering/300x600/main.js
+++ preview/DFL_Doorlopende_Reisverzerkering/300x600/main.js
@@ -56,6 +56,8 @@ function init() {
 }
 
 function getAnimation(){
+
+    createSnow();
     
     var splitT1 = new SplitText("#introText", {type: "words"});
 
@@ -67,6 +69,7 @@ function getAnimation(){
     introTL.from("#op1", 0.2, {scale:0, ease:Sine.easeOut})
     introTL.from("#op2", 0.2, {scale:0, ease:Sine.easeOut})
     introTL.from("#stickerWrapper", 0.5, {scale:0, rotation:-300, ease:Back.easeOut},"+=1")
+    introTL.to("#snowWrapper", 0.5, {opacity:0, ease:Sine.easeOut}, "start+=14.5");
 }
 
 function getUSPAnimation(){
@@ -75,9 +78,10 @@ function getUSPAnimation(){
 
     uspTL = new TimelineLite({});
     uspTL.add("start")
-    uspTL.to("#intro", 0.5, {scale:0, ease:Sine.easeOut}, "start")
     uspTL.to("#stickerWrapper", 0.5, {scale:0, opacity:0, rotation:-300, ease:Sine.easeOut},"start")
-    uspTL.to("#uspScreen", 0.5, {scale:1, ease:Sine.easeInOut});
+    uspTL.to("#uspScreen", 0.01, {opacity:1, display: "block", ease:Sine.easeIn}, "start");
+    uspTL.to("#intro", 0.01, {opacity:0, ease:Sine.easeOut}, "start+=0.01")
+    uspTL.from("#uspScreen", 0.5, {height:244, ease:Sine.easeInOut});
     uspTL.from(splitT1.words, 0.5, {opacity: 0, x:-20, ease: Sine.easeOut, stagger: 0.05});
     uspTL.from("#usp1", 0.5, {x:-20, opacity:0, ease: Sine.easeOut});
     uspTL.from("#usp2", 0.5, {x:-20, opacity:0, ease: Sine.easeOut});
@@ -101,52 +105,51 @@ function getUSPAnimation(){
 }
 
 function getQuestionAnimation(){
+    
+    var splitT1 = new SplitText("#questionText", {type: "words"});
+
+    questionTL = new TimelineLite({});
+    questionTL.add("start")
+    questionTL.to("#stickerWrapper", 0.5, {scale:0, opacity:0, rotation:-300, ease:Sine.easeOut},"start")
+    questionTL.to("#questionScreen", 0.01, {opacity:1, display: "block", ease:Sine.easeIn}, "start");
+    questionTL.to("#intro", 0.01, {opacity:0, ease:Sine.easeOut}, "start+=0.01")
+    questionTL.from("#questionScreen", 0.5, {height:244, ease:Sine.easeInOut});
+    questionTL.to("#stickerWrapper", 0.5, {scale:1, opacity:1, y:-130, rotation:0, ease:Sine.easeOut})
+    questionTL.from(splitT1.words, 0.5, {opacity: 0, x:-20, ease: Sine.easeOut, stagger: 0.05});
+    questionTL.from(document.querySelectorAll(".question")[0], 0.5, {x:-20, opacity:0, ease: Sine.easeOut});
+    questionTL.from("#op3", 0.2, {scale:0, ease:Sine.easeOut})
+    questionTL.from("#op4", 0.2, {scale:0, ease:Sine.easeOut})
+    questionTL.from(document.querySelectorAll(".question")[1], 0.5, {x:-20, opacity:0, ease: Sine.easeOut});
+    questionTL.from("#op5", 0.2, {scale:0, ease:Sine.easeOut})
+    questionTL.from("#op6", 0.2, {scale:0, ease:Sine.easeOut})
+    questionTL.from(document.querySelectorAll(".question")[2], 0.5, {x:-20, opacity:0, ease: Sine.easeOut});
+    questionTL.from("#op7", 0.2, {scale:0, ease:Sine.easeOut})
+    questionTL.from("#op8", 0.2, {scale:0, ease:Sine.easeOut})
+    questionTL.from("#disclaimer2", 0.2, {opacity:0, ease: Sine.easeOut}, "+=0.3");
+    questionTL.from("#cta2", 0.5, {scale:0, ease: Back.easeOut});
+
+    document.querySelector("#questionScreen").addEventListener("mouseover", function(){
+        if(questionTL.progress() == 1){
+            gsap.to("#cta", 0.3, {scale:1.05});
+        }
+    })
+    document.querySelector("#questionScreen").addEventListener("mouseout", function(){
+        if(questionTL.progress() == 1){
+            gsap.to("#cta", 0.3, {scale:1});
+        }
+    })
 
     document.querySelector("#cta2").addEventListener("click", function(){
         Enabler.exit("CTA Exit");
     })
     document.querySelector("#cta2").addEventListener("mouseover", function(){
-        if(uspTL.progress() == 1){
+        if(questionTL.progress() == 1){
             gsap.to("#cta2", 0.3, {scale:1.05});
         }
     })
     document.querySelector("#cta2").addEventListener("mouseout", function(){
-        if(uspTL.progress() == 1){
+        if(questionTL.progress() == 1){
             gsap.to("#cta2", 0.3, {scale:1});
-        }
-    })
-    
-    var splitT1 = new SplitText("#questionText", {type: "words"});
-
-    console.log(document.querySelectorAll(".question")[0]);
-
-    uspTL = new TimelineLite({});
-    uspTL.add("start")
-    uspTL.to("#intro", 0.5, {scale:0, ease:Sine.easeOut}, "start")
-    uspTL.to("#stickerWrapper", 0.5, {scale:0, opacity:0, rotation:-300, ease:Sine.easeOut},"start")
-    uspTL.to("#questionScreen", 0.5, {scale:1, ease:Sine.easeInOut});
-    uspTL.to("#stickerWrapper", 0.5, {scale:1, opacity:1, y:-130, rotation:0, ease:Sine.easeOut})
-    uspTL.from(splitT1.words, 0.5, {opacity: 0, x:-20, ease: Sine.easeOut, stagger: 0.05});
-    uspTL.from(document.querySelectorAll(".question")[0], 0.5, {x:-20, opacity:0, ease: Sine.easeOut});
-    uspTL.from("#op3", 0.2, {scale:0, ease:Sine.easeOut})
-    uspTL.from("#op4", 0.2, {scale:0, ease:Sine.easeOut})
-    uspTL.from(document.querySelectorAll(".question")[1], 0.5, {x:-20, opacity:0, ease: Sine.easeOut});
-    uspTL.from("#op5", 0.2, {scale:0, ease:Sine.easeOut})
-    uspTL.from("#op6", 0.2, {scale:0, ease:Sine.easeOut})
-    uspTL.from(document.querySelectorAll(".question")[2], 0.5, {x:-20, opacity:0, ease: Sine.easeOut});
-    uspTL.from("#op7", 0.2, {scale:0, ease:Sine.easeOut})
-    uspTL.from("#op8", 0.2, {scale:0, ease:Sine.easeOut})
-    uspTL.from("#disclaimer2", 0.2, {opacity:0, ease: Sine.easeOut}, "+=0.3");
-    uspTL.from("#cta2", 0.5, {scale:0, ease: Back.easeOut});
-
-    document.querySelector("#questionScreen").addEventListener("mouseover", function(){
-        if(uspTL.progress() == 1){
-            gsap.to("#cta", 0.3, {scale:1.05});
-        }
-    })
-    document.querySelector("#questionScreen").addEventListener("mouseout", function(){
-        if(uspTL.progress() == 1){
-            gsap.to("#cta", 0.3, {scale:1});
         }
     })
 }
@@ -225,3 +228,39 @@ function calcPrices(){
 }
 
 function isOdd(num) { return num % 2;}
+
+function createSnow(){
+    var wrapper = document.querySelector("#snowWrapper");
+    for (var i = 0; i < 700; i++) {
+        var newDiv = document.createElement("div");
+        var size = gsap.utils.random(1, 4);
+        newDiv.style.width = size+"px";
+        newDiv.style.height = size+"px";
+        newDiv.style.opacity = gsap.utils.random(0.3, 0.9);
+        newDiv.style.left = gsap.utils.random(0, 300)+"px";
+        newDiv.style.top = gsap.utils.random(-600, 600)+"px";
+        newDiv.classList.add("snow");
+        wrapper.appendChild(newDiv);
+
+        var rand = gsap.utils.random(5, 7.5);
+        gsap.to(newDiv, rand, {y:gsap.utils.random(600, 1200), ease:Power0.easeNone, onComplete:animateAgain});
+        gsap.to(newDiv, rand, {x:gsap.utils.random(-250, 250), ease:Power0.easeNone})
+        gsap.to(newDiv, rand, {scale:gsap.utils.random(0.5, 1.2), ease:Power0.easeNone})
+    }
+}
+
+function animateAgain(){
+
+    gsap.fromTo(this._targets[0], 7.5, {
+        y:-600, 
+        x:gsap.utils.random(0, 300), 
+        scale:1
+    }, 
+    {
+        y:gsap.utils.random(600, 1200), 
+        x:gsap.utils.random(-250, 250), 
+        scale:gsap.utils.random(0.5, 1.2), ease:Power0.easeNone
+    }
+    );
+    
+}
