bf11fbc5ed914bcc51add1c4cee05d8af1e67691
Dave Tervoort <dave.tervoort@accenture.com>
2024-01-17 15:07:23 +0100

    Tesa master

---
+++ preview/Tesa_localisations/300x600_tesa_2024_germany/localDynamic.js
@@ -0,0 +1,34 @@
+localDynamicData = {
+	"variationName": "germany",
+	"campaignName": "tesa_2024",
+	"text1": {
+		"default": "Zuverlässiger <span class='blueLineWrapper'>Halt<div class='blueLine'></div></span><BR>ohne Bohren.",
+		"300x600": ""
+	},
+	"text2": {
+		"default": "Hol‘ dir die tesa® Klebenägel!",
+		"300x600": ""
+	},
+	"text3": {
+		"default": "Einfach. Sicher. Ohne Löcher.",
+		"300x600": ""
+	},
+	"ctaText": {
+		"default": "Jetzt kaufen",
+		"300x600": ""
+	},
+	"logo": "./logo.svg",
+	"tadaa1": "./tadaa_t.svg",
+	"tadaa2": "./tadaa_a.svg",
+	"tadaa3": "./tadaa_d.svg",
+	"tadaa4": "./tadaa_a.svg",
+	"tadaa5": "./tadaa_a.svg",
+	"tadaa6": "./tadaa_questionmark.svg",
+	"tesa": "./tesa.svg",
+	"tadaa": "./tadaa.svg",
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"product": "./product.png",
+	"productLeft": "./product_left.png",
+	"productRight": "./product_right.png"
+}
