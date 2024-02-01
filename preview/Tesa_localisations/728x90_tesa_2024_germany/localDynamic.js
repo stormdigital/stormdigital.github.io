a63ac1c7d0e0a4be459d4adae496431953cd344a
Dave Tervoort <dave.tervoort@accenture.com>
2024-02-01 15:35:30 +0100

    728x90 done

---
+++ preview/Tesa_localisations/728x90_tesa_2024_germany/localDynamic.js
@@ -0,0 +1,46 @@
+localDynamicData = {
+	"variationName": "germany",
+	"campaignName": "tesa_2024",
+	"text1": {
+		"default": "Zuverlässiger <span class='blueLineWrapper'>Halt<div class='blueLine'></div></span><BR>ohne Bohren.",
+		"300x600": "",
+		"160x600": "Zuverlässiger<br><span class='blueLineWrapper'>Halt<div class='blueLine'></div></span> ohne<BR>Bohren.",
+		"300x250": "",
+		"728x90": "Zuverlässiger <span class='blueLineWrapper'>Halt<div class='blueLine'></div></span> ohne Bohren."
+	},
+	"text2": {
+		"default": "Hol‘ dir die tesa® Klebenägel!",
+		"300x600": "",
+		"160x600": "Hol‘ dir<br>die tesa®<br><span class='blueLineWrapper'>Klebenägel!<div class='blueLine'></div></span>",
+		"300x250": "Hol‘ dir die tesa® <span class='blueLineWrapper'>Klebenägel!<div class='blueLine'></div></span>",
+		"728x90": "Hol‘ dir die tesa® <span class='blueLineWrapper'>Klebenägel!<div class='blueLine'></div></span>"
+	},
+	"text3": {
+		"default": "Einfach. Sicher. Ohne Löcher.",
+		"300x600": "",
+		"160x600": "Einfach.<br>Sicher.<br><span class='blueLineWrapper'>Ohne<div class='blueLine'></div></span> Löcher.",
+		"300x250": "Einfach. Sicher. <span class='blueLineWrapper'>Ohne<div class='blueLine'></div></span> Löcher.",
+		"728x90": "Einfach. Sicher. <span class='blueLineWrapper'>Ohne<div class='blueLine'></div></span> Löcher."
+	},
+	"ctaText": {
+		"default": "Jetzt kaufen",
+		"300x600": "Jetzt kaufen",
+		"160x600": "Jetzt kaufen",
+		"300x250": "Jetzt kaufen",
+		"728x90": "Jetzt kaufen"
+	},
+	"logo": "./logo.svg",
+	"tadaa1": "./tadaa_t.svg",
+	"tadaa2": "./tadaa_a.svg",
+	"tadaa3": "./tadaa_d.svg",
+	"tadaa4": "./tadaa_a.svg",
+	"tadaa5": "./tadaa_a.svg",
+	"tadaa6": "./tadaa_questionmark.svg",
+	"tesa": "./tesa.svg",
+	"tadaa": "./tadaa.svg",
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"product": "./product_middle.png",
+	"productLeft": "./product_left.png",
+	"productRight": "./product_right.png"
+}
