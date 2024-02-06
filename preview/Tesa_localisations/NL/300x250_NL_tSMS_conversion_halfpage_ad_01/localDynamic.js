12cba9fc4773a663233efa5c5e832e09ff4d614c
Dave Tervoort <dave.tervoort@accenture.com>
2024-02-06 16:54:48 +0100

    Tesa NL

---
+++ preview/Tesa_localisations/NL/300x250_NL_tSMS_conversion_halfpage_ad_01/localDynamic.js
@@ -0,0 +1,46 @@
+localDynamicData = {
+	"variationName": "halfpage_ad_01",
+	"campaignName": "NL_tSMS_conversion",
+	"text1": {
+		"default": "",
+		"300x600": "Decoreer<br>je muren <span class='blueLineWrapper'>zonder<div class='blueLine'></div></span><BR>te hoeven boren.",
+		"160x600": "Decoreer<br>je muren<br><span class='blueLineWrapper'>zonder<div class='blueLine'></div></span><BR>te hoeven boren.",
+		"300x250": "<div style='font-size: 21px; position: relative; line-height: 21px;'>Decoreer je muren<br><span class='blueLineWrapper'>zonder<div class='blueLine'></div></span> te hoeven boren.</div>",
+		"728x90": "Decoreer je muren<br><span class='blueLineWrapper'>zonder<div class='blueLine'></div></span> te hoeven boren."
+	},
+	"text2": {
+		"default": "",
+		"300x600": "Koop<br>klevende spijkers van tesa®.",
+		"160x600": "Koop<br><span class='blueLineWrapper'>klevende<br>spijkers<div class='blueLine'></div></span><br>van tesa®.",
+		"300x250": "Koop <span class='blueLineWrapper'>klevende spijkers<div class='blueLine'></div></span> van tesa®.",
+		"728x90": "Koop <span class='blueLineWrapper'>klevende spijkers<div class='blueLine'></div></span><br>van tesa®."
+	},
+	"text3": {
+		"default": "",
+		"300x600": "Makkelijk. Veilig.<br>Zonder gaatjes.",
+		"160x600": "Makkelijk.<br>Veilig.<br><span class='blueLineWrapper'>Zonder<div class='blueLine'></div></span><br>gaatjes.",
+		"300x250": "Makkelijk. Veilig. <span class='blueLineWrapper'>Zonder gaatjes.<div class='blueLine'></div></span>",
+		"728x90": "Makkelijk. Veilig.<br><span class='blueLineWrapper'>Zonder gaatjes.<div class='blueLine'></div></span>"
+	},
+	"ctaText": {
+		"default": "Koop nu",
+		"300x600": "Koop nu",
+		"160x600": "Koop nu",
+		"300x250": "Koop nu",
+		"728x90": "Koop nu"
+	},
+	"logo": "./logo.svg",
+	"tadaa1": "./tadaa_t.svg",
+	"tadaa2": "./tadaa_a.svg",
+	"tadaa3": "./tadaa_d.svg",
+	"tadaa4": "./tadaa_a.svg",
+	"tadaa5": "./tadaa_a.svg",
+	"tadaa6": "./tadaa_questionmark.svg",
+	"tesa": "./tesa.svg",
+	"tadaa": "./tadaa.svg",
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"product": "./product_middle.png",
+	"productLeft": "./product_left.png",
+	"productRight": "./product_right.png"
+}
