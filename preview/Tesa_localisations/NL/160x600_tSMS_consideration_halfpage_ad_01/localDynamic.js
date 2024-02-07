ee2f76398ccddd78a17beb9f54714f66a39a0d25
Dave Tervoort <dave.tervoort@accenture.com>
2024-02-07 12:09:23 +0100

    Tesa consideration

---
+++ preview/Tesa_localisations/NL/160x600_tSMS_consideration_halfpage_ad_01/localDynamic.js
@@ -0,0 +1,53 @@
+localDynamicData = {
+	"variationName": "halfpage_ad_01",
+	"campaignName": "tSMS_consideration",
+	"text1": {
+		"default": "Zuverlässiger <span class='blueLineWrapper'>Halt<div class='blueLine'></div></span><BR>ohne Bohren.",
+		"300x600": "",
+		"160x600": "Decoreer<br>je muren<br><span class='blueLineWrapper'>zonder<div class='blueLine'></div></span><BR>te hoeven boren.",
+		"300x250": "<div style='font-size: 21px; position: relative; line-height: 21px;'>Decoreer je muren<br><span class='blueLineWrapper'>zonder<div class='blueLine'></div></span> te hoeven boren.</div>",
+		"728x90": "Decoreer je muren<br><span class='blueLineWrapper'>zonder<div class='blueLine'></div></span> te hoeven boren."
+	},
+	"text2": {
+		"default": "Hol‘ dir die tesa® Klebenägel!",
+		"300x600": "",
+		"160x600": "<span class='blueLineWrapper'>Gewoon<br>spijkers<div class='blueLine'></div></span><br>kleven en<br>aandrukken.",
+		"300x250": "<div style='font-size: 21px; position: relative; line-height: 21px;'><span class='blueLineWrapper'>Gewoon<div class='blueLine'></div></span> kleven<br>en aandrukken.</div>",
+		"728x90": "<span class='blueLineWrapper'>Gewoon<div class='blueLine'></div></span> kleven<br>en aandrukken."
+	},
+	"disclaimer": {
+		"default": "",
+		"300x600": "",
+		"160x600": "",
+		"300x250": "",
+		"728x90": ""
+	},
+	"text3": {
+		"default": "",
+		"300x600": "",
+		"160x600": "<div style='font-size: 20px; position: relative; line-height: 20px;'><span class='blueLineWrapper'>Klevende<div class='blueLine'></div></span><br><span class='blueLineWrapper'>spijkers<div class='blueLine'></div></span><br>van tesa®.<br>Makkelijk.<br>Veilig.<br>Zonder gaatjes.</div>",
+		"300x250": "<div style='font-size: 18px; position: relative; line-height: 18px;'><span class='blueLineWrapper'>Klevende spijkers<div class='blueLine'></div></span> van tesa®.<br>Makkelijk. Veilig. Zonder gaatjes.</div>",
+		"728x90": "<span class='blueLineWrapper'>Klevende spijkers<div class='blueLine'></div></span> van tesa®.<br>Makkelijk. Veilig. Zonder gaatjes."
+	},
+	"ctaText": {
+		"default": "Meer informatie",
+		"300x600": "Meer informatie",
+		"160x600": "Meer informatie",
+		"300x250": "Meer informatie",
+		"728x90": "Meer informatie"
+	},
+	"logo": "./logo.svg",
+	"tadaa1": "./tadaa_t.svg",
+	"tadaa2": "./tadaa_a.svg",
+	"tadaa3": "./tadaa_d.svg",
+	"tadaa4": "./tadaa_a.svg",
+	"tadaa5": "./tadaa_a.svg",
+	"tadaa6": "./tadaa_questionmark.svg",
+	"tesa": "./tesa.svg",
+	"tadaa": "./tadaa.svg",
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"product": "./product_middle.png",
+	"productLeft": "./product_left.png",
+	"productRight": "./product_right.png"
+}
---
+++ preview/Tesa_localisations/NL/160x600_tSMS_consideration_halfpage_ad_03/localDynamic.js
@@ -0,0 +1,53 @@
+localDynamicData = {
+	"variationName": "halfpage_ad_03",
+	"campaignName": "tSMS_consideration",
+	"text1": {
+		"default": "",
+		"300x600": "",
+		"160x600": "Elke fotolijst<br><span class='blueLineWrapper'>stevig<div class='blueLine'></div></span><BR>aan de muur.",
+		"300x250": "<div style='font-size: 21px; position: relative; line-height: 21px;'>Elke fotolijst<br><span class='blueLineWrapper'>stevig<div class='blueLine'></div></span> aan de muur.</div>",
+		"728x90": "Elke fotolijst<br><span class='blueLineWrapper'>stevig<div class='blueLine'></div></span> aan de muur."
+	},
+	"text2": {
+		"default": "",
+		"300x600": "",
+		"160x600": "Zelfs op<br><span class='blueLineWrapper'>pleisterwerk<div class='blueLine'></div></span>,<br>behang<br>en tegelwerk.",
+		"300x250": "<div style='font-size: 21px; position: relative; line-height: 21px;'>Zelfs op <span class='blueLineWrapper'>pleisterwerk<div class='blueLine'></div></span>,<br>behang en tegelwerk.</div>",
+		"728x90": "Zelfs op <span class='blueLineWrapper'>pleisterwerk<div class='blueLine'></div></span>,<br>behang en tegelwerk."
+	},
+	"disclaimer": {
+		"default": "",
+		"300x600": "",
+		"160x600": "",
+		"300x250": "",
+		"728x90": ""
+	},
+	"text3": {
+		"default": "Einfach. Sicher. Ohne Löcher.",
+		"300x600": "",
+		"160x600": "<div style='font-size: 20px; position: relative; line-height: 20px;'><span class='blueLineWrapper'>Klevende<div class='blueLine'></div></span><br><span class='blueLineWrapper'>spijkers<div class='blueLine'></div></span><br>van tesa®.<br>Makkelijk.<br>Veilig.<br>Zonder gaatjes.</div>",
+		"300x250": "<div style='font-size: 18px; position: relative; line-height: 18px;'><span class='blueLineWrapper'>Klevende spijkers<div class='blueLine'></div></span> van tesa®.<br>Makkelijk. Veilig. Zonder gaatjes.</div>",
+		"728x90": "<span class='blueLineWrapper'>Klevende spijkers<div class='blueLine'></div></span> van tesa®.<br>Makkelijk. Veilig. Zonder gaatjes."
+	},
+	"ctaText": {
+		"default": "Meer informatie",
+		"300x600": "Meer informatie",
+		"160x600": "Meer informatie",
+		"300x250": "Meer informatie",
+		"728x90": "<div style='font-size: 11px; line-height: 25px; position: relative;'>Meer informatie</div>"
+	},
+	"logo": "./logo.svg",
+	"tadaa1": "./tadaa_t.svg",
+	"tadaa2": "./tadaa_a.svg",
+	"tadaa3": "./tadaa_d.svg",
+	"tadaa4": "./tadaa_a.svg",
+	"tadaa5": "./tadaa_a.svg",
+	"tadaa6": "./tadaa_questionmark.svg",
+	"tesa": "./tesa.svg",
+	"tadaa": "./tadaa.svg",
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"product": "./product_middle.png",
+	"productLeft": "./product_left.png",
+	"productRight": "./product_right.png"
+}
