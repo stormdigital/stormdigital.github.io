97a86e528a1f7096630b00d78556953bffbdc65e
Dave Tervoort <dave.tervoort@accenture.com>
2024-02-08 15:11:51 +0100

    loader color of video ad and removed test preview

--- preview/test/list_directories.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Get all directories in the current directory
-$directories = array_filter(glob('*'), 'is_dir');
-
-// Output directories separated by newline character
-echo implode("\n", $directories);
-?>
