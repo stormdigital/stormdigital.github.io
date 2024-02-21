eed0077767f1f0324b9a495be9036dbd972bdfc7
Dave Tervoort <dave.tervoort@accenture.com>
2024-02-21 16:17:07 +0100

    dremel

--- preview/Dremel_Multiest_tool/Multiest_Tool_8240-5_Think_Evergreen_HTML5_300x600_v2_UK_English/main.js
+++ preview/Dremel_Multiest_tool/Multiest_Tool_8240-5_Think_Evergreen_HTML5_300x600_v2_UK_English/main.js
@@ -102,7 +102,7 @@ function getAnimation(){
     masterTL.from("#logo", 0.5, {width:200, ease:Sine.easeOut}, "start+=0.5")
     masterTL.from("#bar", 0.1, {width:0, margin:0, ease:Sine.easeInOut}, "start+=1")
     masterTL.from("#taglineWrapper", 1, {width:0, ease:Power2.easeOut}, "start+=1.1")
-    masterTL.add("showOneTool", "start+=1")
+    masterTL.add("showOneTool", "start+=1.5")
     masterTL.from("#oneTool1", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool")
     masterTL.from("#oneTool2", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.1")
     masterTL.from("#oneTool3", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.2")
@@ -110,10 +110,9 @@ function getAnimation(){
     masterTL.from("#oneTool5", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.4")
     masterTL.from("#oneTool6", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.5")
     masterTL.from("#oneTool7", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.6")
-    masterTL.add("showProduct5", "-=1")
+    masterTL.add("showProduct5", "+=0.1")
     masterTL.from("#product", 0.5, {x:100, ease:Back.easeOut}, "showProduct5")
     for(var i=1; i<6; i++){
-        console.log(i);
         masterTL.from("#tool"+i+" .toolText", 0.5, {opacity:0, ease:Sine.easeOut}, "showProduct5+="+(0.1*i))
         masterTL.from("#tool"+i+" .toolImage", 0.5, {opacity:0, stagger:0.1, ease:Sine.easeOut}, "showProduct5+="+(0.1*i))
     }
@@ -123,11 +122,7 @@ function getAnimation(){
         masterTL.to("#tool"+i, 0.3, {opacity:0, ease:Power2.easeOut}, "-=0.5")
         masterTL.to("#tool"+(i+5), 0.3, {opacity:1, ease:Power2.easeIn}, "-=0.5")
     }
-    masterTL.to("#banner", 0.1, {}, 7.4)
+    masterTL.to("#banner", 0.1, {}, 8.9)
 
-    console.log(masterTL.duration());
-    
-    
-    // masterTL.to("#toolsWrapper", 2, {y:-186, ease:Sine.easeOut}, "showProduct+=4")
-
+    masterTL.duration(7.5)
 }
