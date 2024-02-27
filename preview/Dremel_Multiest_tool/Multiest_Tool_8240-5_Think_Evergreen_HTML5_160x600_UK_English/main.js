d4996e8cce46a35c985f8a0832bc18502381528e
Dave Tervoort <dave.tervoort@accenture.com>
2024-02-27 15:08:06 +0100

    Dremel resizes

--- preview/Dremel_Multiest_tool/Multiest_Tool_8240-5_Think_Evergreen_HTML5_160x600_UK_English/main.js
+++ preview/Dremel_Multiest_tool/Multiest_Tool_8240-5_Think_Evergreen_HTML5_160x600_UK_English/main.js
@@ -3,7 +3,7 @@ window.onload = function () {
 }
 
 function loadLocalDynamic() {
-    var adSize = "300x600";
+    var adSize = "160x600";
     if(localDynamicData.tagline[adSize] != ""){
         document.querySelector('#tagline').innerHTML = localDynamicData.tagline[adSize];
     }
@@ -100,8 +100,7 @@ function getAnimation(){
         gsap.set("#loaderWrapper", {display:"none"});
     }}, 0)
     masterTL.add("start");
-    masterTL.from("#bottomWrapper", 0.5, {bottom:"50%", y:"50%", ease:Sine.easeOut}, "start+=0.5")
-    masterTL.from("#logo", 0.5, {width:200, ease:Sine.easeOut}, "start+=0.5")
+    masterTL.from("#logo", 0.5, {width:100, bottom:"50%", y:"50%", ease:Sine.easeOut}, "start+=0.5")
     masterTL.from("#bar", 0.1, {width:0, margin:0, ease:Sine.easeInOut}, "start+=1")
     masterTL.from("#taglineWrapper", 1, {width:0, ease:Power2.easeOut}, "start+=1.1")
     masterTL.add("showOneTool", "start+=1.5")
@@ -113,14 +112,14 @@ function getAnimation(){
     masterTL.from("#oneTool6", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.5")
     masterTL.from("#oneTool7", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.6")
     masterTL.add("showProduct5", "+=0.1")
-    masterTL.from("#product", 0.5, {x:100, ease:Back.easeOut}, "showProduct5")
+    masterTL.from("#product", 0.5, {x:40, ease:Back.easeOut}, "showProduct5")
     for(var i=1; i<6; i++){
         masterTL.from("#tool"+i+" .toolText", 0.5, {opacity:0, ease:Sine.easeOut}, "showProduct5+="+(0.1*i))
         masterTL.from("#tool"+i+" .toolImage", 0.5, {opacity:0, stagger:0.1, ease:Sine.easeOut}, "showProduct5+="+(0.1*i))
     }
     masterTL.add("caroussel")
     for(var i=1; i<6; i++){
-        masterTL.to("#toolsWrapper", 0.5, {y:"+=90", ease:Sine.easeInOut}, "caroussel+="+(i-1)*1)
+        masterTL.to("#toolsWrapper", 0.5, {y:"+=80", ease:Sine.easeInOut}, "caroussel+="+(i-1)*1)
         masterTL.to("#tool"+i, 0.3, {opacity:0, ease:Power2.easeOut}, "-=0.5")
         masterTL.to("#tool"+(i+5), 0.3, {opacity:1, ease:Power2.easeIn}, "-=0.5")
     }
--- preview/Dremel_Multiest_tool/Multiest_Tool_8240-5_Think_Evergreen_HTML5_728x90_UK_English/main.js
+++ preview/Dremel_Multiest_tool/Multiest_Tool_8240-5_Think_Evergreen_HTML5_728x90_UK_English/main.js
@@ -3,57 +3,57 @@ window.onload = function () {
 }
 
 function loadLocalDynamic() {
-    var adSize = "300x600";
-    if(localDynamicData.tagline[adSize] != ""){
-        document.querySelector('#tagline').innerHTML = localDynamicData.tagline[adSize];
-    }
-    else{
-        document.querySelector('#tagline').innerHTML = localDynamicData.tagline["default"];
-    }
+    var adSize = "300x250";
+    // if(localDynamicData.tagline[adSize] != ""){
+    //     document.querySelector('#tagline').innerHTML = localDynamicData.tagline[adSize];
+    // }
+    // else{
+    //     document.querySelector('#tagline').innerHTML = localDynamicData.tagline["default"];
+    // }
     
     if(localDynamicData.tool1Text[adSize] != ""){
         document.querySelector('#tool1 .toolText').innerHTML = localDynamicData.tool1Text[adSize];
-        document.querySelector('#tool6 .toolText').innerHTML = localDynamicData.tool1Text[adSize];
+        // document.querySelector('#tool6 .toolText').innerHTML = localDynamicData.tool1Text[adSize];
     }
     else{
         document.querySelector('#tool1 .toolText').innerHTML = localDynamicData.tool1Text["default"];
-        document.querySelector('#tool6 .toolText').innerHTML = localDynamicData.tool1Text["default"];
+        // document.querySelector('#tool6 .toolText').innerHTML = localDynamicData.tool1Text["default"];
     }
     
     if(localDynamicData.tool2Text[adSize] != ""){
         document.querySelector('#tool2 .toolText').innerHTML = localDynamicData.tool2Text[adSize];
-        document.querySelector('#tool7 .toolText').innerHTML = localDynamicData.tool2Text[adSize];
+        // document.querySelector('#tool7 .toolText').innerHTML = localDynamicData.tool2Text[adSize];
     }
     else{
         document.querySelector('#tool2 .toolText').innerHTML = localDynamicData.tool2Text["default"];
-        document.querySelector('#tool7 .toolText').innerHTML = localDynamicData.tool2Text["default"];
+        // document.querySelector('#tool7 .toolText').innerHTML = localDynamicData.tool2Text["default"];
     }
     
     if(localDynamicData.tool3Text[adSize] != ""){
         document.querySelector('#tool3 .toolText').innerHTML = localDynamicData.tool3Text[adSize];
-        document.querySelector('#tool8 .toolText').innerHTML = localDynamicData.tool3Text[adSize];
+        // document.querySelector('#tool8 .toolText').innerHTML = localDynamicData.tool3Text[adSize];
     }
     else{
         document.querySelector('#tool3 .toolText').innerHTML = localDynamicData.tool3Text["default"];
-        document.querySelector('#tool8 .toolText').innerHTML = localDynamicData.tool3Text["default"];
+        // document.querySelector('#tool8 .toolText').innerHTML = localDynamicData.tool3Text["default"];
     }
     
     if(localDynamicData.tool4Text[adSize] != ""){
         document.querySelector('#tool4 .toolText').innerHTML = localDynamicData.tool4Text[adSize];
-        document.querySelector('#tool9 .toolText').innerHTML = localDynamicData.tool4Text[adSize];
+        // document.querySelector('#tool9 .toolText').innerHTML = localDynamicData.tool4Text[adSize];
     }
     else{
         document.querySelector('#tool4 .toolText').innerHTML = localDynamicData.tool4Text["default"];
-        document.querySelector('#tool9 .toolText').innerHTML = localDynamicData.tool4Text["default"];
+        // document.querySelector('#tool9 .toolText').innerHTML = localDynamicData.tool4Text["default"];
     }
     
     if(localDynamicData.tool5Text[adSize] != ""){
         document.querySelector('#tool5 .toolText').innerHTML = localDynamicData.tool5Text[adSize];
-        document.querySelector('#tool10 .toolText').innerHTML = localDynamicData.tool5Text[adSize];
+        // document.querySelector('#tool10 .toolText').innerHTML = localDynamicData.tool5Text[adSize];
     }
     else{
         document.querySelector('#tool5 .toolText').innerHTML = localDynamicData.tool5Text["default"];
-        document.querySelector('#tool10 .toolText').innerHTML = localDynamicData.tool5Text["default"];
+        // document.querySelector('#tool10 .toolText').innerHTML = localDynamicData.tool5Text["default"];
     }
 
     document.querySelector('#logo').data = localDynamicData.logo;
@@ -62,11 +62,11 @@ function loadLocalDynamic() {
     document.querySelector('#tool3 .toolImage').src = localDynamicData.tool3Img;
     document.querySelector('#tool4 .toolImage').src = localDynamicData.tool4Img;
     document.querySelector('#tool5 .toolImage').src = localDynamicData.tool5Img;
-    document.querySelector('#tool6 .toolImage').src = localDynamicData.tool1Img;
-    document.querySelector('#tool7 .toolImage').src = localDynamicData.tool2Img;
-    document.querySelector('#tool8 .toolImage').src = localDynamicData.tool3Img;
-    document.querySelector('#tool9 .toolImage').src = localDynamicData.tool4Img;
-    document.querySelector('#tool10 .toolImage').src = localDynamicData.tool5Img;
+    // document.querySelector('#tool6 .toolImage').src = localDynamicData.tool1Img;
+    // document.querySelector('#tool7 .toolImage').src = localDynamicData.tool2Img;
+    // document.querySelector('#tool8 .toolImage').src = localDynamicData.tool3Img;
+    // document.querySelector('#tool9 .toolImage').src = localDynamicData.tool4Img;
+    // document.querySelector('#tool10 .toolImage').src = localDynamicData.tool5Img;
 }
 
 function init(clickTAGvalue) {
@@ -100,10 +100,10 @@ function getAnimation(){
         gsap.set("#loaderWrapper", {display:"none"});
     }}, 0)
     masterTL.add("start");
-    masterTL.from("#bottomWrapper", 0.5, {bottom:"50%", y:"50%", ease:Sine.easeOut}, "start+=0.5")
-    masterTL.from("#logo", 0.5, {width:200, ease:Sine.easeOut}, "start+=0.5")
-    masterTL.from("#bar", 0.1, {width:0, margin:0, ease:Sine.easeInOut}, "start+=1")
-    masterTL.from("#taglineWrapper", 1, {width:0, ease:Power2.easeOut}, "start+=1.1")
+    // masterTL.from("#bottomWrapper", 0.5, {ease:Sine.easeOut}, "start+=0.5")
+    masterTL.from("#logo", 0.5, {width:200, bottom:"50%", y:"50%", left:"50%", x:"-50%", ease:Sine.easeOut}, "start+=0.5")
+    // masterTL.from("#bar", 0.1, {width:0, margin:0, ease:Sine.easeInOut}, "start+=1")
+    // masterTL.from("#taglineWrapper", 1, {width:0, ease:Power2.easeOut}, "start+=1.1")
     masterTL.add("showOneTool", "start+=1.5")
     masterTL.from("#oneTool1", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool")
     masterTL.from("#oneTool2", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.1")
@@ -113,17 +113,17 @@ function getAnimation(){
     masterTL.from("#oneTool6", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.5")
     masterTL.from("#oneTool7", 0.5, {scale:0, ease:Power2.easeOut}, "showOneTool+=0.6")
     masterTL.add("showProduct5", "+=0.1")
-    masterTL.from("#product", 0.5, {x:100, ease:Back.easeOut}, "showProduct5")
+    masterTL.from("#product", 0.5, {y:100, ease:Back.easeOut}, "showProduct5")
     for(var i=1; i<6; i++){
-        masterTL.from("#tool"+i+" .toolText", 0.5, {opacity:0, ease:Sine.easeOut}, "showProduct5+="+(0.1*i))
-        masterTL.from("#tool"+i+" .toolImage", 0.5, {opacity:0, stagger:0.1, ease:Sine.easeOut}, "showProduct5+="+(0.1*i))
+        masterTL.from("#tool"+i+" .toolText", 0.5, {opacity:0, ease:Sine.easeOut}, "showProduct5+="+(0.3*(i+1)))
+        masterTL.from("#tool"+i+" .toolImage", 0.5, {opacity:0, stagger:0.1, ease:Sine.easeOut}, "showProduct5+="+(0.3*(i+1)))
     }
-    masterTL.add("caroussel")
-    for(var i=1; i<6; i++){
-        masterTL.to("#toolsWrapper", 0.5, {y:"+=90", ease:Sine.easeInOut}, "caroussel+="+(i-1)*1)
-        masterTL.to("#tool"+i, 0.3, {opacity:0, ease:Power2.easeOut}, "-=0.5")
-        masterTL.to("#tool"+(i+5), 0.3, {opacity:1, ease:Power2.easeIn}, "-=0.5")
-    }
+    // masterTL.add("caroussel")
+    // for(var i=1; i<6; i++){
+    //     masterTL.to("#toolsWrapper", 0.5, {x:"-=60", ease:Sine.easeInOut}, "caroussel+="+(i-1)*1)
+    //     masterTL.to("#tool"+i, 0.3, {opacity:0, ease:Power2.easeOut}, "-=0.5")
+    //     masterTL.to("#tool"+(i+5), 0.3, {opacity:1, ease:Power2.easeIn}, "-=0.5")
+    // }
     masterTL.to("#banner", 0.1, {}, 8.9)
 
     masterTL.duration(7.5)
