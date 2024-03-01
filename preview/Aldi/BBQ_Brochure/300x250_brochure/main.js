d42a52fdabe9f0cf1f22e842f36a409bbcb371f0
Dave Tervoort <dave.tervoort@accenture.com>
2024-03-01 16:15:18 +0100

    BBQ brochure

---
+++ preview/Aldi/BBQ_Brochure/300x250_brochure/main.js
@@ -0,0 +1,53 @@
+myFT.on("ready", init);
+
+
+function init() {
+
+    document.querySelector("#mainExit").addEventListener("mouseover", function(){
+        if(masterTL.progress() == 1){
+            gsap.to("#ctaRight", 0.3, {opacity:0.8, ease:Sine.easeInOut});
+        }
+    })
+    
+    document.querySelector("#mainExit").addEventListener("mouseout", function(){
+        if(masterTL.progress() == 1){
+            gsap.to("#ctaRight", 0.3, {opacity:1, ease:Sine.easeInOut});
+        }
+    })
+
+    document.querySelector("#mainExit").addEventListener("click", function(){
+        masterTL.pause();
+        // masterTL.progress(1);
+    })
+
+    getAnimation();
+}
+
+function getAnimation(){
+
+    masterTL = gsap.timeline({paused:true});
+    masterTL.add("start")
+    masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut}, 0)
+    masterTL.add("start", "-=0.2");
+    masterTL.from("#text1", 0.5, {x:300, ease:Sine.easeOut}, "start+=0.3")
+    masterTL.from("#text1HighlightWrapper", 0.7, {width:0, ease:Sine.easeInOut}, "start+=1.3")
+    masterTL.to(["#text1", "#text1HighlightWrapper"], 0.3, {opacity:0, ease:Sine.easeInOut}, "start+=3.7")
+    masterTL.to("#introBg", 0.3, {opacity:0, ease:Sine.easeIn}, "start+=3.7")
+    masterTL.from("#brochure", 1, {width: 721, y:-53, x: -274, rotation:-4, ease:Power2.easeInOut}, "start+=3.7")
+    masterTL.from("#topic1", 0.5, {x:-200, ease:Sine.easeOut}, "start+=4.5")
+    masterTL.from("#topic2", 0.5, {x:-200, ease:Sine.easeOut}, "start+=4.7")
+    masterTL.from("#topic3", 0.5, {x:-200, ease:Sine.easeOut}, "start+=4.9")
+    masterTL.to("#brochure", 4, {rotation:-4, ease:Sine.easeInOut}, "start+=4.7")
+    masterTL.to(["#logo", "#topic1", "#topic2", "#topic3", "#brochure"], 0.5, {opacity:0, ease:Sine.easeInOut}, "8.2")
+    masterTL.add("endscreen", "-=0.1");
+    masterTL.from("#logo2", 0.3, {opacity:0, ease:Sine.easeOut}, "endscreen")
+    masterTL.from("#tagline", 0.3, {opacity:0, ease:Sine.easeOut}, "endscreen+=0.1")
+    masterTL.from("#endText", 0.5, {x:-300, ease:Sine.easeOut}, "endscreen+=0.2")
+    masterTL.from("#ctaLeft", 0.5, {scale:0, transformOrigin:"50% 95%", ease:Back.easeOut}, "endscreen+=1");
+    masterTL.from("#ctaRight", 0.01, {opacity:0, ease:Sine.easeOut}, "endscreen+=1.7");
+    masterTL.from("#ctaRight", 0.3, {x:-50, ease:Sine.easeOut}, "endscreen+=1.7");
+ 
+    masterTL.play();
+
+    console.log(masterTL.duration());
+}
