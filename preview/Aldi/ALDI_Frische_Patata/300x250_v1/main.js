cce9d199a144386841b37971030c2c40444fc0be
Dave Tervoort <dave.tervoort@accenture.com>
2024-03-22 17:22:11 +0100

    patattas master feedback Aldi

--- preview/Aldi/ALDI_Frische_Patata/300x250_v1/main.js
+++ preview/Aldi/ALDI_Frische_Patata/300x250_v1/main.js
@@ -38,17 +38,17 @@ function getAnimation(){
     masterTL.to("#holeWrapper", 1.4, {x:94, y:32, ease:Sine.easeInOut}, "holeAnimation+=1.8")
     masterTL.to("#holeWrapper", 0.6, {x:0, y:0, ease:Sine.easeIn}, "holeAnimation+=3.6")
     masterTL.to("#holeWrapper", 0.6, {scale:5, ease:Sine.easeIn}, "holeAnimation+=3.9")
-    masterTL.from("#text1", 0.5, {opacity:0, x:-50, ease:Sine.easeOut}, "holeAnimation+=4.5")
+    masterTL.from("#text1", 0.5, {opacity:0, ease:Sine.easeOut}, "holeAnimation+=3.9")
     masterTL.add("showEnd", "+=1.8")
     masterTL.from("#endBg", 1, {clipPath: "circle(0% at 50% 50%)", ease:"none"}, "showEnd")
-    masterTL.from("#text2", 0.5, {opacity:0, x:-50, ease:Sine.easeOut}, "showEnd+=0.2")
-    masterTL.from("#endCircle", 1, {scale:0, ease:Sine.easeIn}, "showEnd+=2.5")
-    masterTL.to("#logo", 0.5, {x:106, width:64, ease:Sine.easeInOut}, "showEnd+=2.5")
-    masterTL.to("#text2", 0.5, {y:16, ease:Sine.easeInOut}, "showEnd+=2.5")
-    masterTL.from("#tagline", 0.5, {opacity:0, ease:Sine.easeOut}, "showEnd+=3")
-    masterTL.from("#ctaLeft", 0.5, {scale:0, transformOrigin:"50% 95%", ease:Back.easeOut}, "showEnd+=3.5");
-    masterTL.from("#ctaRight", 0.01, {opacity:0, ease:Sine.easeOut}, "showEnd+=4.2");
-    masterTL.from("#ctaRight", 0.3, {x:-50, ease:Sine.easeOut}, "showEnd+=4.2");
+    masterTL.from("#text2", 0.5, {opacity:0, x:-50, ease:Sine.easeOut}, "showEnd+=0.8")
+    masterTL.from("#endCircle", 1, {scale:0, ease:Sine.easeIn}, "showEnd+=3")
+    masterTL.to("#logo", 0.5, {x:106, width:64, ease:Sine.easeInOut}, "showEnd+=3")
+    masterTL.to("#text2", 0.5, {y:16, ease:Sine.easeInOut}, "showEnd+=3")
+    masterTL.from("#tagline", 0.5, {opacity:0, ease:Sine.easeOut}, "showEnd+=3.5")
+    masterTL.from("#ctaLeft", 0.5, {scale:0, transformOrigin:"50% 95%", ease:Back.easeOut}, "showEnd+=4");
+    masterTL.from("#ctaRight", 0.01, {opacity:0, ease:Sine.easeOut}, "showEnd+=4.7");
+    masterTL.from("#ctaRight", 0.3, {x:-50, ease:Sine.easeOut}, "showEnd+=4.7");
  
     masterTL.play();
 
