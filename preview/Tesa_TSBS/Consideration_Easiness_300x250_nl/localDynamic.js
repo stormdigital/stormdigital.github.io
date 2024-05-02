9d1646c555fa1cd04b68b7405287cfb3c8174302
Dave Tervoort <dave.tervoort@accenture.com>
2024-05-02 10:40:58 +0200

    Tesa master

---
+++ preview/Tesa_TSBS/Consideration_Easiness_300x250_nl/localDynamic.js
@@ -0,0 +1,58 @@
+localDynamicData = {
+	"variationName": "GER_01",
+	"campaignName": "nl",
+	"dimensionName": {
+		"200x600": "Consideration_Easiness",
+		"300x250": "Consideration_Easiness",
+		"728x90": "Consideration_Easiness"
+	},
+	"text1": {
+		"default": "Ganz <span class='blueLineWrapper'>einfach<div class='blueLine'></div></span>.",
+		"200x600": "",
+		"160x600": "",
+		"300x250": "",
+		"728x90": ""
+	},
+	"text2": {
+		"default": "<span class='blueLineWrapper'>Ohne<div class='blueLine'></div></span> Bohren.",
+		"200x600": "",
+		"160x600": "",
+		"300x250": "",
+		"728x90": ""
+	},
+	"disclaimer": {
+		"default": "",
+		"200x600": "",
+		"160x600": "",
+		"300x250": "",
+		"728x90": ""
+	},
+	"text3": {
+		"default": "tesa® Bad Designs.<BR><span class='blueLineWrapper'>Sicher<div class='blueLine'></div></span> geklebt.",
+		"200x600": "",
+		"160x600": "",
+		"300x250": "",
+		"728x90": ""
+	},
+	"ctaText": {
+		"default": "Mehr Infos",
+		"200x600": "",
+		"160x600": "",
+		"300x250": "",
+		"728x90": ""
+	},
+	"logo": "./logo.svg",
+	"tadaa1": "./tadaa_t.svg",
+	"tadaa2": "./tadaa_a.svg",
+	"tadaa3": "./tadaa_d.svg",
+	"tadaa4": "./tadaa_a.svg",
+	"tadaa5": "./tadaa_a.svg",
+	"tadaa6": "./tadaa_questionmark.svg",
+	"tesa": "./tesa.svg",
+	"tadaa": "./tadaa.svg",
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"product": "./product_middle.png",
+	"productLeft": "./product_left.png",
+	"productRight": "./product_right.png"
+}
---
+++ preview/Tesa_TSBS/Consideration_Easiness_300x250_nl/main.js
@@ -0,0 +1,137 @@
+window.onload = function () {
+    init();
+}
+
+function loadLocalDynamic() {
+    var adSize = "300x250";
+    if(localDynamicData.text1[adSize] != ""){
+        document.querySelector('#text1').innerHTML = localDynamicData.text1[adSize];
+    }
+    else{
+        document.querySelector('#text1').innerHTML = localDynamicData.text1["default"];
+    }
+    
+    if(localDynamicData.text2[adSize] != ""){
+        document.querySelector('#text2').innerHTML = localDynamicData.text2[adSize];
+    }
+    else{
+        document.querySelector('#text2').innerHTML = localDynamicData.text2["default"];
+    }
+    
+    if(localDynamicData.text3[adSize] != ""){
+        document.querySelector('#text3').innerHTML = localDynamicData.text3[adSize];
+    }
+    else{
+        document.querySelector('#text3').innerHTML = localDynamicData.text3["default"];
+    }
+    
+    if(localDynamicData.disclaimer[adSize] != ""){
+        document.querySelector('#disclaimer').innerHTML = localDynamicData.disclaimer[adSize];
+    }
+    else{
+        document.querySelector('#disclaimer').innerHTML = localDynamicData.disclaimer["default"];
+    }
+    
+    if(localDynamicData.ctaText[adSize] != ""){
+        document.querySelector('#cta').innerHTML = localDynamicData.ctaText[adSize];
+    }
+    else{
+        document.querySelector('#cta').innerHTML = localDynamicData.ctaText["default"];
+    }
+    
+    if(localDynamicData.ctaText[adSize] != ""){
+        document.querySelector('#ctaBorder').innerHTML = localDynamicData.ctaText[adSize];
+    }
+    else{
+        document.querySelector('#ctaBorder').innerHTML = localDynamicData.ctaText["default"];
+    }
+
+    document.querySelector('#logo').data = localDynamicData.logo;
+    document.querySelector('#tesa').data = localDynamicData.tesa;
+    document.querySelector('#tadaa1').data = localDynamicData.tadaa1;
+    document.querySelector('#tadaa2').data = localDynamicData.tadaa2;
+    document.querySelector('#tadaa3').data = localDynamicData.tadaa3;
+    document.querySelector('#tadaa4').data = localDynamicData.tadaa4;
+    document.querySelector('#tadaa5').data = localDynamicData.tadaa5;
+    document.querySelector('#tadaa6').data = localDynamicData.tadaa6;
+    document.querySelector('#bg1').src = localDynamicData.bg1;
+    document.querySelector('#bg2').src = localDynamicData.bg2;
+    document.querySelector('#mainProduct').src = localDynamicData.product;
+    document.querySelector('#leftProduct').src = localDynamicData.productLeft;
+    document.querySelector('#rightProduct').src = localDynamicData.productRight;
+
+}
+
+function init(clickTAGvalue) {
+    loadLocalDynamic();
+
+    document.querySelector("#mainExit").addEventListener("mouseover", function(){
+        if(masterTL.progress() == 1){
+            
+        }
+    })
+    
+    document.querySelector("#mainExit").addEventListener("mouseout", function(){
+        if(masterTL.progress() == 1){
+            
+        }
+    })
+
+    document.querySelector("#mainExit").addEventListener("click", function(){
+        window.open(clickTag, '_blank');
+        masterTL.progress(1).pause();
+    })
+
+    getAnimation();
+}
+
+function getAnimation(){
+
+    masterTL = new TimelineLite({repeat:1});
+    masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut, onComplete:function(){
+        gsap.set("#loaderWrapper", {display:"none"});
+    }}, 0)
+    masterTL.add("start");
+    masterTL.from("#bg1", 10, {scale:1.2, ease:Sine.easeOut}, "start")
+    masterTL.from("#text1", 0.7, {x:-50, opacity:0, ease:Power2.easeInOut}, "start+=0.2")
+    document.querySelectorAll("#text1 .blueLine").forEach(blueline => {
+        masterTL.from(blueline, 0.5, {width:0, ease:Power2.easeInOut}, "start+=0.9")
+    });
+    masterTL.to("#text1", 0.7, {x:50, opacity:0, ease:Power2.easeInOut}, "start+=3.5")
+    masterTL.from("#text2", 0.7, {x:-50, opacity:0, ease:Power2.easeInOut}, "start+=4")
+    masterTL.from("#disclaimer", 0.5, {opacity:0, ease:Power2.easeInOut}, "start+=4.2")
+    document.querySelectorAll("#text2 .blueLine").forEach(blueline => {
+        masterTL.from(blueline, 0.5, {width:0, ease:Power2.easeInOut}, "start+=4.7")
+    });
+    masterTL.to("#text2", 0.7, {x:50, opacity:0, ease:Power2.easeInOut}, "start+=7")
+    masterTL.to("#disclaimer", 0.5, {opacity:0, ease:Power2.easeInOut}, "start+=7.2")
+    masterTL.from("#bg2", 5, {scale:1.2, ease:Sine.easeOut}, "start+=7")
+    masterTL.from("#bg2", 0.5, {opacity:0, ease:Sine.easeInOut}, "start+=7")
+    masterTL.from("#tesaWrapper", 0.5, {width:0, ease:Sine.easeInOut}, "start+=7.5")
+    masterTL.from("#tadaa1", 0.5, {y:150, scale:0, x:80, ease:Back.easeOut}, "start+=7.7")
+    masterTL.from("#tadaa2", 0.5, {y:150, scale:0, x:44, ease:Back.easeOut}, "start+=7.76")
+    masterTL.from("#tadaa3", 0.5, {y:150, scale:0, x:6, ease:Back.easeOut}, "start+=7.8")
+    masterTL.from("#tadaa4", 0.5, {y:150, scale:0, x:-30, ease:Back.easeOut}, "start+=7.85")
+    masterTL.from("#tadaa5", 0.5, {y:150, scale:0, x:-70, ease:Back.easeOut}, "start+=7.9")
+    masterTL.from("#tadaa6", 0.5, {y:150, scale:0, x:-100, ease:Back.easeOut}, "start+=7.95")
+    masterTL.to("#tesaWrapper", 0.5, {opacity:0, x:70, ease:Sine.easeIn}, "start+=9.5")
+    masterTL.to("#tadaaWrapper", 0.5, {opacity:0, x:70, ease:Sine.easeIn}, "start+=9.5")
+    masterTL.from("#blueCircle", 1.25, {scaleY:0, ease:"elastic.out(0.5,0.3)"}, "start+=10")
+    masterTL.from("#mainProduct", 1, {opacity:0, y:70, ease:"elastic.out(0.5 ,0.4)"}, "start+=10")
+    masterTL.to(".blur", 0.8, {opacity: 1, ease:Sine.easeInOut}, "start+=10")
+    masterTL.from(["#cta", "#ctaBorder"], 1, {opacity:0, y:70, ease:"elastic.out(0.5 ,0.4)"}, "start+=10.5")
+    masterTL.from("#leftProduct", 0.4, {x:70, opacity:0, ease:Back.easeOut}, "start+=10.5")
+    masterTL.from("#rightProduct", 0.4, {x:-70, opacity:0, ease:Back.easeOut}, "start+=10.5")
+    masterTL.from("#text3", 0.7, {x:-50, opacity:0, ease:Power2.easeInOut}, "start+=11")
+    document.querySelectorAll("#text3 .blueLine").forEach(blueline => {
+        masterTL.from(blueline, 0.5, {width:0, ease:Power2.easeInOut}, "start+=11.7")
+    });
+    masterTL.to("#cta", 0.25, {scale:1.15, ease:Sine.easeInOut, repeat:1, yoyo:true}, "start+=13")
+    masterTL.to("#ctaBorder", 0.5, {scale:1.8, ease:Sine.easeIn, opacity:0}, "start+=13")
+    masterTL.to("#ctaBorder", 0.001, {scale:1, opacity:1}, "start+=13.999")
+    masterTL.to("#cta", 0.25, {scale:1.15, ease:Sine.easeInOut, repeat:1, yoyo:true}, "start+=14")
+    masterTL.to("#ctaBorder", 0.5, {scale:1.8, ease:Sine.easeIn, opacity:0}, "start+=14")
+    masterTL.to("#ctaBorder", 0.5, {opacity:0}, "start+=14.5")
+
+    masterTL.duration(13.5);
+}
