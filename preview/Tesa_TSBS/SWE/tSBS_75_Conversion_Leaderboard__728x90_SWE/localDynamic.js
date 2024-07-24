c7c40a8b7eb8cc262d807759a75dc4391d83ae13
Dave Tervoort <dave.tervoort@accenture.com>
2024-07-24 10:28:37 +0200

    Tesa restructure + SWE versions done

---
+++ preview/Tesa_TSBS/SWE/tSBS_75_Conversion_Leaderboard__728x90_SWE/localDynamic.js
@@ -0,0 +1,31 @@
+localDynamicData = {
+	"variationName": "SWE_02",
+	"campaignName": "SWE",
+	"dimensionName": {
+		"300x250": "tSBS_69_Conversion_MediumRectangle_",
+		"728x90": "tSBS_75_Conversion_Leaderboard_"
+	},
+	"text1": "./text1.png",
+	"highlight1": "./highlight1.png",
+	"text2": "./text2.png",
+	"highlight2": "./highlight2.png",
+	"text3": "./text3.png",
+	"highlight3": "./highlight3.png",
+	"logo": "./logo.svg",
+	"tadaa1": "./tadaa_t.svg",
+	"tadaa2": "./tadaa_a.svg",
+	"tadaa3": "./tadaa_d.svg",
+	"tadaa4": "./tadaa_a.svg",
+	"tadaa5": "./tadaa_a.svg",
+	"tadaa6": "./tadaa_questionmark.svg",
+	"tesa": "./tesa.svg",
+	"tadaa": "./tadaa.svg",
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"bg3": "./bg3.jpg",
+	"product": "./product_middle.png",
+	"productLeft": "./product_left.png",
+	"productRight": "./product_right.png",
+	"cta": "./cta.png",
+	"ctaBorder": "./cta_border.png"
+}
