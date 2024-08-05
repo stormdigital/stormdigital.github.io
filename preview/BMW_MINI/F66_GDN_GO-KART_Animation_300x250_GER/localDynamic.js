34f58fde99cdebfe0559d5b85ddfb001e43c755d
Dave Tervoort <dave.tervoort@accenture.com>
2024-08-05 16:41:25 +0200

    mini masters

---
+++ preview/BMW_MINI/F66_GDN_GO-KART_Animation_300x250_GER/localDynamic.js
@@ -0,0 +1,189 @@
+localDynamicData = {
+	"variationName": "GER_01",
+	"campaignName": "GER",
+	"dimensionName": {
+		"300x600": "F66_GDN_GO-KART_Animation",
+		"300x250": "F66_GDN_GO-KART_Animation",
+		"970x250": "F66_GDN_GO-KART_Animation"
+	},
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"logoMini": "./mini_logo.svg",
+	"logoJCW": "./jcw_logo.svg",
+	"text1a": {
+		"300x600": {
+			"text": "THE NEW",
+			"size": 43,
+			"top": 38
+		},
+		"300x250": {
+			"text": "THE NEW",
+			"size": 35,
+			"top": 8
+		},
+		"970x250": {
+			"text": "THE NEW",
+			"size": 36,
+			"top": 13
+		}
+	},
+	"text1b": {
+		"300x600": {
+			"text": "MINI john cooper works.",
+			"size": 15,
+			"top": 81
+		},
+		"300x250": {
+			"text": "MINI john cooper works.",
+			"size": 15,
+			"top": 44
+		},
+		"970x250": {
+			"text": "MINI john cooper works.",
+			"size": 17,
+			"top": 50
+		}
+	},
+	"text1c": {
+		"300x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"text1d": {
+		"300x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"cta1": {
+		"300x600": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": 106,
+			"height": 23
+		},
+		"300x250": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": 70,
+			"height": 23
+		},
+		"970x250": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": 83,
+			"height": 23
+		}
+	},
+	"text2a": {
+		"300x600": {
+			"text": "READY. SET.",
+			"size": 40,
+			"top": 39
+		},
+		"300x250": {
+			"text": "READY. SET.",
+			"size": 35,
+			"top": 8
+		},
+		"970x250": {
+			"text": "READY. SET.",
+			"size": 36,
+			"top": 15
+		}
+	},
+	"text2b": {
+		"300x600": {
+			"text": "GOOOO-KART.",
+			"size": 40,
+			"top": 74
+		},
+		"300x250": {
+			"text": "GOOOO-KART.",
+			"size": 35,
+			"top": 36
+		},
+		"970x250": {
+			"text": "GOOOO-KART.",
+			"size": 36,
+			"top": 44
+		}
+	},
+	"text2c": {
+		"300x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"text2d": {
+		"300x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"cta2": {
+		"300x600": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": 125,
+			"height": 23
+		},
+		"300x250": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": 83,
+			"height": 23
+		},
+		"970x250": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": 90,
+			"height": 24
+		}
+	}
+}
