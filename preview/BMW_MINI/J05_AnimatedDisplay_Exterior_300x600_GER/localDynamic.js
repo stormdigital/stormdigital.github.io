8fe970bc71a78b5c71a4de77fc9d606da63a49af
Dave Tervoort <dave.tervoort@accenture.com>
2024-08-05 20:33:22 +0200

    J05 masters

---
+++ preview/BMW_MINI/J05_AnimatedDisplay_Exterior_300x600_GER/localDynamic.js
@@ -0,0 +1,189 @@
+localDynamicData = {
+	"variationName": "GER_02",
+	"campaignName": "GER",
+	"dimensionName": {
+		"300x600": "J05_AnimatedDisplay_Exterior",
+		"300x250": "J05_AnimatedDisplay_Exterior",
+		"970x250": "J05_AnimatedDisplay_Exterior"
+	},
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"logoMini": "./mini_logo.svg",
+	"logoJCW": "./jcw_logo.svg",
+	"text1a": {
+		"300x600": {
+			"text": "ACCELERATE",
+			"size": 29,
+			"top": 84
+		},
+		"300x250": {
+			"text": "ACCELERATE",
+			"size": 28,
+			"top": 14
+		},
+		"970x250": {
+			"text": "ACCELERATE",
+			"size": 36,
+			"top": 14
+		}
+	},
+	"text1b": {
+		"300x600": {
+			"text": "0-100 KM/H IN",
+			"size": 29,
+			"top": 113
+		},
+		"300x250": {
+			"text": "0-100 KM/H IN 6.5s.",
+			"size": 28,
+			"top": 43
+		},
+		"970x250": {
+			"text": "0-100 KM/H IN 6.5s.",
+			"size": 36,
+			"top": 50
+		}
+	},
+	"text1c": {
+		"300x600": {
+			"text": "6.5s.",
+			"size": 29,
+			"top": 143
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"text1d": {
+		"300x600": {
+			"text": "",
+			"size": 5,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"cta1": {
+		"300x600": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -106,
+			"height": 23
+		},
+		"300x250": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -200,
+			"height": 23
+		},
+		"970x250": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -283,
+			"height": 23
+		}
+	},
+	"text2a": {
+		"300x600": {
+			"text": "THE FIRST EVER",
+			"size": 29,
+			"top": 84
+		},
+		"300x250": {
+			"text": "THE FIRST EVER",
+			"size": 26,
+			"top": 14
+		},
+		"970x250": {
+			"text": "THE FIRST EVER ALL-ELECTRIC",
+			"size": 36,
+			"top": 14
+		}
+	},
+	"text2b": {
+		"300x600": {
+			"text": "ALL-ELECTRIC",
+			"size": 29,
+			"top": 113
+		},
+		"300x250": {
+			"text": "ALL-ELECTRIC",
+			"size": 26,
+			"top": 43
+		},
+		"970x250": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 36,
+			"top": 50
+		}
+	},
+	"text2c": {
+		"300x600": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 29,
+			"top": 143
+		},
+		"300x250": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 26,
+			"top": 71
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"text2d": {
+		"300x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"cta2": {
+		"300x600": {
+			"text": "DISCOVER NOW",
+			"size": 13,
+			"top": 188,
+			"height": 23
+		},
+		"300x250": {
+			"text": "DISCOVER NOW",
+			"size": 13,
+			"top": 110,
+			"height": 23
+		},
+		"970x250": {
+			"text": "DISCOVER NOW",
+			"size": 13,
+			"top": 100,
+			"height": 24
+		}
+	}
+}
