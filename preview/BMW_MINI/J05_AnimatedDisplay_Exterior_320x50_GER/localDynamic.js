2707bdc97cc9b3ccba290b44227b92d0f374fa18
Dave Tervoort <dave.tervoort@accenture.com>
2024-08-06 13:20:05 +0200

    Mini resizes

---
+++ preview/BMW_MINI/J05_AnimatedDisplay_Exterior_320x50_GER/localDynamic.js
@@ -0,0 +1,348 @@
+localDynamicData = {
+	"variationName": "GER_02",
+	"campaignName": "GER",
+	"dimensionName": {
+		"300x600": "J05_AnimatedDisplay_Exterior",
+		"160x600": "J05_AnimatedDisplay_Exterior",
+		"300x250": "J05_AnimatedDisplay_Exterior",
+		"320x50": "J05_AnimatedDisplay_Exterior",
+		"728x90": "J05_AnimatedDisplay_Exterior",
+		"970x250": "J05_AnimatedDisplay_Exterior"
+	},
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"logoMini": "./mini_logo.svg",
+	"logoJCW": "./jcw_logo.svg",
+	"text1a": {
+		"300x600": {
+			"text": "ACCELERATE",
+			"size": 29,
+			"top": 84
+		},
+		"160x600": {
+			"text": "ACCELERATE",
+			"size": 19,
+			"top": 48
+		},
+		"300x250": {
+			"text": "ACCELERATE",
+			"size": 28,
+			"top": 14
+		},
+		"320x50": {
+			"text": "ACCELERATE",
+			"size": 15,
+			"top": 8
+		},
+		"728x90": {
+			"text": "ACCELERATE",
+			"size": 25,
+			"top": 6
+		},
+		"970x250": {
+			"text": "ACCELERATE",
+			"size": 36,
+			"top": 14
+		}
+	},
+	"text1b": {
+		"300x600": {
+			"text": "0-100 KM/H IN",
+			"size": 29,
+			"top": 113
+		},
+		"160x600": {
+			"text": "0-100 KM/H IN",
+			"size": 19,
+			"top": 71
+		},
+		"300x250": {
+			"text": "0-100 KM/H IN 6.5s.",
+			"size": 28,
+			"top": 43
+		},
+		"320x50": {
+			"text": "0-100 KM/H IN 6.5s.",
+			"size": 15,
+			"top": 23
+		},
+		"728x90": {
+			"text": "0-100 KM/H IN",
+			"size": 25,
+			"top": 30
+		},
+		"970x250": {
+			"text": "0-100 KM/H IN 6.5s.",
+			"size": 36,
+			"top": 50
+		}
+	},
+	"text1c": {
+		"300x600": {
+			"text": "6.5s.",
+			"size": 29,
+			"top": 143
+		},
+		"160x600": {
+			"text": "6.5s.",
+			"size": 19,
+			"top": 94
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x50": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"728x90": {
+			"text": "6.5s.",
+			"size": 25,
+			"top": 54
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"text1d": {
+		"300x600": {
+			"text": "",
+			"size": 5,
+			"top": 0
+		},
+		"160x600": {
+			"text": "",
+			"size": 5,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x50": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"728x90": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"cta1": {
+		"300x600": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -106,
+			"height": 23
+		},
+		"160x600": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -106,
+			"height": 23
+		},
+		"300x250": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -200,
+			"height": 23
+		},
+		"320x50": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -200,
+			"height": 23
+		},
+		"728x90": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -200,
+			"height": 23
+		},
+		"970x250": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -283,
+			"height": 23
+		}
+	},
+	"text2a": {
+		"300x600": {
+			"text": "THE FIRST EVER",
+			"size": 29,
+			"top": 84
+		},
+		"160x600": {
+			"text": "THE FIRST EVER",
+			"size": 15,
+			"top": 54
+		},
+		"300x250": {
+			"text": "THE FIRST EVER",
+			"size": 26,
+			"top": 14
+		},
+		"320x50": {
+			"text": "THE FIRST EVER ALL-ELECTRIC",
+			"size": 12,
+			"top": 5
+		},
+		"728x90": {
+			"text": "THE FIRST EVER ALL-ELECTRIC",
+			"size": 19,
+			"top": 10
+		},
+		"970x250": {
+			"text": "THE FIRST EVER ALL-ELECTRIC",
+			"size": 36,
+			"top": 14
+		}
+	},
+	"text2b": {
+		"300x600": {
+			"text": "ALL-ELECTRIC",
+			"size": 29,
+			"top": 113
+		},
+		"160x600": {
+			"text": "ALL-ELECTRIC",
+			"size": 15,
+			"top": 70
+		},
+		"300x250": {
+			"text": "ALL-ELECTRIC",
+			"size": 26,
+			"top": 43
+		},
+		"320x50": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 12,
+			"top": 16
+		},
+		"728x90": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 19,
+			"top": 28
+		},
+		"970x250": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 36,
+			"top": 50
+		}
+	},
+	"text2c": {
+		"300x600": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 29,
+			"top": 143
+		},
+		"160x600": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 15,
+			"top": 87
+		},
+		"300x250": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 26,
+			"top": 71
+		},
+		"320x50": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 26,
+			"top": 71
+		},
+		"728x90": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"text2d": {
+		"300x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"160x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x50": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"728x90": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"cta2": {
+		"300x600": {
+			"text": "DISCOVER NOW",
+			"size": 13,
+			"top": 188,
+			"height": 23
+		},
+		"160x600": {
+			"text": "DISCOVER NOW",
+			"size": 11,
+			"top": 111,
+			"height": 20
+		},
+		"300x250": {
+			"text": "DISCOVER NOW",
+			"size": 13,
+			"top": 110,
+			"height": 23
+		},
+		"320x50": {
+			"text": "DISCOVER NOW",
+			"size": 7,
+			"top": 32,
+			"height": 13
+		},
+		"728x90": {
+			"text": "DISCOVER NOW",
+			"size": 13,
+			"top": 55,
+			"height": 22
+		},
+		"970x250": {
+			"text": "DISCOVER NOW",
+			"size": 13,
+			"top": 100,
+			"height": 24
+		}
+	}
+}
---
+++ preview/BMW_MINI/J05_AnimatedDisplay_Exterior_728x90_GER/main.js
@@ -0,0 +1,109 @@
+window.onload = function () {
+    init();
+}
+
+function loadLocalDynamic(text1Letters, adSize) {
+
+    for (var i = 0; i < text1Letters.length; i++) {
+        document.querySelector('#text1'+text1Letters[i]).innerHTML = localDynamicData["text1"+text1Letters[i]][adSize].text;
+        document.querySelector('#text1'+text1Letters[i]).style.fontSize = localDynamicData["text1"+text1Letters[i]][adSize].size+"px";
+        document.querySelector('#text1'+text1Letters[i]).style.top = localDynamicData["text1"+text1Letters[i]][adSize].top+"px";
+        
+        document.querySelector('#text2'+text1Letters[i]).innerHTML = localDynamicData["text2"+text1Letters[i]][adSize].text;
+        document.querySelector('#text2'+text1Letters[i]).style.fontSize = localDynamicData["text2"+text1Letters[i]][adSize].size+"px";
+        document.querySelector('#text2'+text1Letters[i]).style.top = localDynamicData["text2"+text1Letters[i]][adSize].top+"px";
+    }
+
+    document.querySelector('#cta1').innerHTML = localDynamicData.cta1[adSize].text;
+    document.querySelector('#cta1').style.fontSize = localDynamicData.cta1[adSize].size+"px";
+    document.querySelector('#cta1Wrapper').style.top = localDynamicData.cta1[adSize].top+"px";
+    document.querySelector('#cta1Wrapper').style.height = localDynamicData.cta1[adSize].height+"px";
+    document.querySelector('#cta1').style.lineHeight = localDynamicData.cta1[adSize].height+"px";
+    
+    document.querySelector('#cta2').innerHTML = localDynamicData.cta2[adSize].text;
+    document.querySelector('#cta2').style.fontSize = localDynamicData.cta2[adSize].size+"px";
+    document.querySelector('#cta2Wrapper').style.top = localDynamicData.cta2[adSize].top+"px";
+    document.querySelector('#cta2Wrapper').style.height = localDynamicData.cta2[adSize].height+"px";
+    document.querySelector('#cta2').style.lineHeight = localDynamicData.cta2[adSize].height+"px";
+    
+    document.querySelector('#bg1').style.backgroundImage = "url("+localDynamicData.bg1+")";
+    document.querySelector('#bg2').style.backgroundImage = "url("+localDynamicData.bg2+")";
+    document.querySelector('#logoMini').src = localDynamicData.logoMini;
+    document.querySelector('#logoJCW').src = localDynamicData.logoJCW;
+}
+
+function init(clickTAGvalue) {
+
+    var adSize = "728x90";
+    var text1Letters = ["a", "b", "c", "d"];
+
+    loadLocalDynamic(text1Letters, adSize);
+
+    document.querySelector("#mainExit").addEventListener("mouseover", function(){
+        if(masterTL.progress() > 0.7){
+            gsap.to("#cta2Wrapper", 0.3, {scale:1.1, ease:Sine.easeInOut}); 
+        }
+
+        gsap.to(["#cta1Wrapper", "#cta2Wrapper"], 0.3, {background:"#fff", color:"#000", ease:Sine.easeInOut});
+
+        gsap.to(["#bg1", "#bg2"], 0.5, {filter: "sepia(0.5)"});
+        gsap.to(["#bg1", "#bg2"], 0.5, {filter: "sepia(0)", delay:0.5});
+    })
+    
+    document.querySelector("#mainExit").addEventListener("mouseout", function(){
+        if(masterTL.progress() > 0.7){
+            gsap.to("#cta2Wrapper", 0.3, {scale:1, ease:Sine.easeInOut});
+        }
+
+        gsap.to(["#cta1Wrapper", "#cta2Wrapper"], 0.3, {background:"#c22e20", color:"#fff", ease:Sine.easeInOut});
+    })
+
+    document.querySelector("#mainExit").addEventListener("click", function(){
+        clickTAGvalue = dhtml.getVar('clickTAG', 'http://www.example.com');
+        // masterTL.progress(1).pause();
+        masterTL.pause();
+    })
+
+    getAnimation(text1Letters, adSize);
+}
+
+function getAnimation(text1Letters, adSize){
+
+    masterTL = new TimelineLite({paused:true});
+    masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut}, 0)
+    masterTL.add("start");
+    masterTL.from("#bg1", 5, {scale:1.05, ease:Sine.easeInOut}, "start")
+    masterTL.from("#logoMini", 0.5, {y:100, ease:Sine.easeOut}, "start+=0.2")
+    masterTL.from("#logoJCW", 0.5, {y:100, ease:Sine.easeOut}, "start+=0.4")
+    masterTL.add("text1Start", "-=4")
+    var delay1 = 0;
+    for (var i = 0; i < text1Letters.length; i++) {
+        if(localDynamicData["text1"+text1Letters[i]][adSize].text != ""){
+            masterTL.from("#text1"+text1Letters[i], 0.5, {opacity:0, y:30, ease:Sine.easeOut}, "text1Start+="+delay1)
+            delay1+=0.15;
+        } 
+    }
+    masterTL.from("#cta1Wrapper", 0.5, {scale:0, ease:Back.easeOut}, "text1Start+="+(delay1+0.5))
+    masterTL.add("text1Remove", "-=0.5")
+    var delay2 = 0;
+    for (var i = 0; i < text1Letters.length; i++) {
+        if(localDynamicData["text1"+text1Letters[i]][adSize].text != ""){
+            masterTL.to("#text1"+text1Letters[i], 0.3, {opacity:0, y:-30, ease:Sine.easeOut}, "text1Remove+="+delay2)
+            delay2+=0.15;
+        }   
+    }
+    masterTL.to("#cta1Wrapper", 0.3, {scale:0, ease:Sine.easeIn}, "text1Remove+="+delay2)
+    masterTL.from("#bg2", 5, {scale:1.05, ease:Sine.easeInOut}, "start+=5")
+    masterTL.to("#bg1", 0.3, {opacity:0, ease:Sine.easeInOut}, "start+=5")
+    masterTL.add("text2Start", "-=4")
+    var delay3 = 0;
+    for (var i = 0; i < text1Letters.length; i++) {
+        if(localDynamicData["text2"+text1Letters[i]][adSize].text != ""){
+            masterTL.from("#text2"+text1Letters[i], 0.5, {opacity:0, y:30, ease:Sine.easeOut}, "text2Start+="+delay3)
+            delay3+=0.15;
+        } 
+    }
+    masterTL.from("#cta2Wrapper", 0.5, {scale:0, ease:Back.easeOut}, "text2Start+="+(delay3+0.5))
+    
+    masterTL.play();
+}
