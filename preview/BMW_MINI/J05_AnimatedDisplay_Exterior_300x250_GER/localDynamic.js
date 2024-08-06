60fe509c674f891f6c62f3512aeafa2b678c8cb3
Dave Tervoort <dave.tervoort@accenture.com>
2024-08-06 15:28:50 +0200

    mini background images update

--- preview/BMW_MINI/J05_AnimatedDisplay_Exterior_300x250_GER/localDynamic.js
+++ preview/BMW_MINI/J05_AnimatedDisplay_Exterior_300x250_GER/localDynamic.js
@@ -3,7 +3,10 @@ localDynamicData = {
 	"campaignName": "GER",
 	"dimensionName": {
 		"300x600": "J05_AnimatedDisplay_Exterior",
+		"160x600": "J05_AnimatedDisplay_Exterior",
 		"300x250": "J05_AnimatedDisplay_Exterior",
+		"320x50": "J05_AnimatedDisplay_Exterior",
+		"728x90": "J05_AnimatedDisplay_Exterior",
 		"970x250": "J05_AnimatedDisplay_Exterior"
 	},
 	"bg1": "./bg1.jpg",
@@ -16,10 +19,25 @@ localDynamicData = {
 			"size": 29,
 			"top": 84
 		},
+		"160x600": {
+			"text": "ACCELERATE",
+			"size": 19,
+			"top": 48
+		},
 		"300x250": {
 			"text": "ACCELERATE",
 			"size": 28,
 			"top": 14
+		},
+		"320x50": {
+			"text": "ACCELERATE",
+			"size": 15,
+			"top": 8
+		},
+		"728x90": {
+			"text": "ACCELERATE",
+			"size": 25,
+			"top": 6
 		},
 		"970x250": {
 			"text": "ACCELERATE",
@@ -33,10 +51,25 @@ localDynamicData = {
 			"size": 29,
 			"top": 113
 		},
+		"160x600": {
+			"text": "0-100 KM/H IN",
+			"size": 19,
+			"top": 71
+		},
 		"300x250": {
 			"text": "0-100 KM/H IN 6.5s.",
 			"size": 28,
 			"top": 43
+		},
+		"320x50": {
+			"text": "0-100 KM/H IN 6.5s.",
+			"size": 15,
+			"top": 23
+		},
+		"728x90": {
+			"text": "0-100 KM/H IN",
+			"size": 25,
+			"top": 30
 		},
 		"970x250": {
 			"text": "0-100 KM/H IN 6.5s.",
@@ -50,10 +83,25 @@ localDynamicData = {
 			"size": 29,
 			"top": 143
 		},
+		"160x600": {
+			"text": "6.5s.",
+			"size": 19,
+			"top": 94
+		},
 		"300x250": {
 			"text": "",
 			"size": 0,
 			"top": 0
+		},
+		"320x50": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"728x90": {
+			"text": "6.5s.",
+			"size": 25,
+			"top": 54
 		},
 		"970x250": {
 			"text": "",
@@ -67,7 +115,22 @@ localDynamicData = {
 			"size": 5,
 			"top": 0
 		},
+		"160x600": {
+			"text": "",
+			"size": 5,
+			"top": 0
+		},
 		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x50": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"728x90": {
 			"text": "",
 			"size": 0,
 			"top": 0
@@ -85,7 +148,25 @@ localDynamicData = {
 			"top": -106,
 			"height": 23
 		},
+		"160x600": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -106,
+			"height": 23
+		},
 		"300x250": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -200,
+			"height": 23
+		},
+		"320x50": {
+			"text": "EXPLORE NOW",
+			"size": 13,
+			"top": -200,
+			"height": 23
+		},
+		"728x90": {
 			"text": "EXPLORE NOW",
 			"size": 13,
 			"top": -200,
@@ -104,10 +185,25 @@ localDynamicData = {
 			"size": 29,
 			"top": 84
 		},
+		"160x600": {
+			"text": "THE FIRST EVER",
+			"size": 15,
+			"top": 54
+		},
 		"300x250": {
 			"text": "THE FIRST EVER",
 			"size": 26,
 			"top": 14
+		},
+		"320x50": {
+			"text": "THE FIRST EVER ALL-ELECTRIC",
+			"size": 12,
+			"top": 5
+		},
+		"728x90": {
+			"text": "THE FIRST EVER ALL-ELECTRIC",
+			"size": 19,
+			"top": 10
 		},
 		"970x250": {
 			"text": "THE FIRST EVER ALL-ELECTRIC",
@@ -121,10 +217,25 @@ localDynamicData = {
 			"size": 29,
 			"top": 113
 		},
+		"160x600": {
+			"text": "ALL-ELECTRIC",
+			"size": 15,
+			"top": 70
+		},
 		"300x250": {
 			"text": "ALL-ELECTRIC",
 			"size": 26,
 			"top": 43
+		},
+		"320x50": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 12,
+			"top": 16
+		},
+		"728x90": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 19,
+			"top": 28
 		},
 		"970x250": {
 			"text": "MINI JCW ACEMAN.",
@@ -138,10 +249,25 @@ localDynamicData = {
 			"size": 29,
 			"top": 143
 		},
+		"160x600": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 15,
+			"top": 87
+		},
 		"300x250": {
 			"text": "MINI JCW ACEMAN.",
 			"size": 26,
 			"top": 71
+		},
+		"320x50": {
+			"text": "MINI JCW ACEMAN.",
+			"size": 26,
+			"top": 71
+		},
+		"728x90": {
+			"text": "",
+			"size": 0,
+			"top": 0
 		},
 		"970x250": {
 			"text": "",
@@ -155,7 +281,22 @@ localDynamicData = {
 			"size": 0,
 			"top": 0
 		},
+		"160x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
 		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x50": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"728x90": {
 			"text": "",
 			"size": 0,
 			"top": 0
@@ -173,11 +314,29 @@ localDynamicData = {
 			"top": 188,
 			"height": 23
 		},
+		"160x600": {
+			"text": "DISCOVER NOW",
+			"size": 11,
+			"top": 111,
+			"height": 20
+		},
 		"300x250": {
 			"text": "DISCOVER NOW",
 			"size": 13,
 			"top": 110,
 			"height": 23
+		},
+		"320x50": {
+			"text": "DISCOVER NOW",
+			"size": 7,
+			"top": 32,
+			"height": 13
+		},
+		"728x90": {
+			"text": "DISCOVER NOW",
+			"size": 13,
+			"top": 55,
+			"height": 22
 		},
 		"970x250": {
 			"text": "DISCOVER NOW",
