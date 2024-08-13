7635b36fd7e971b13cc8cf3aa9cb5cced4008342
Fatih <fatih.yalcin@accenture.com>
2024-08-13 11:32:36 +0200

    f66 exterior done

---
+++ preview/BMW_MINI/F66/F66_AnimatedDisplay_Exterior_728x90_GER/localDynamic.js
@@ -0,0 +1,632 @@
+localDynamicData = {
+	"variationName": "F66_02",
+	"campaignName": "GER",
+	"dimensionName": {
+		"300x600": "F66_AnimatedDisplay_Exterior",
+		"160x600": "F66_AnimatedDisplay_Exterior",
+		"320x480": "F66_AnimatedDisplay_Exterior",
+		"320x320": "F66_AnimatedDisplay_Exterior",
+		"300x250": "F66_AnimatedDisplay_Exterior",
+		"320x50": "F66_AnimatedDisplay_Exterior",
+		"320x100": "F66_AnimatedDisplay_Exterior",
+		"728x90": "F66_AnimatedDisplay_Exterior",
+		"800x250": "F66_AnimatedDisplay_Exterior",
+		"970x250": "F66_AnimatedDisplay_Exterior"
+	},
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"logoMini": "./mini_logo.svg",
+	"logoJCW": "./jcw_logo.svg",
+	"scale": {
+		"300x600": {
+			"bg1": 1.15,
+			"bg2": 1.05,
+			"introScale": 1.15,
+			"transOr1": "50% 80%",
+			"transOr2": "50% 80%"
+		},
+		"160x600": {
+			"bg1": 1.15,
+			"bg2": 1.05,
+			"introScale": 1.15,
+			"transOr1": "50% 50%",
+			"transOr2": "50% 50%"
+		},
+		"320x480": {
+			"bg1": 1.15,
+			"bg2": 1.05,
+			"introScale": 1.15,
+			"transOr1": "50% 50%",
+			"transOr2": "50% 50%"
+		},
+		"320x320": {
+			"bg1": 1.15,
+			"bg2": 1.05,
+			"introScale": 1.15,
+			"transOr1": "50% 60%",
+			"transOr2": "50% 60%"
+		},
+		"300x250": {
+			"bg1": 1.15,
+			"bg2": 1.05,
+			"introScale": 1.15,
+			"transOr1": "50% 50%",
+			"transOr2": "50% 80%"
+		},
+		"320x50": {
+			"bg1": 1.1,
+			"bg2": 1.05,
+			"introScale": 1.1,
+			"transOr1": "50% 50%",
+			"transOr2": "50% 50%"
+		},
+		"320x100": {
+			"bg1": 1.2,
+			"bg2": 1.05,
+			"introScale": 1.2,
+			"transOr1": "50% 50%",
+			"transOr2": "50% 50%"
+		},
+		"728x90": {
+			"bg1": 1.15,
+			"bg2": 1.05,
+			"introScale": 1.15,
+			"transOr1": "50% 50%",
+			"transOr2": "50% 50%"
+		},
+		"800x250": {
+			"bg1": 1.15,
+			"bg2": 1.05,
+			"introScale": 1.15,
+			"transOr1": "40% 40%",
+			"transOr2": "40% 40%"
+		},
+		"970x250": {
+			"bg1": 1.15,
+			"bg2": 1.05,
+			"introScale": 1.15,
+			"transOr1": "50% 50%",
+			"transOr2": "50% 50%"
+		}
+	},
+	"text2a": {
+		"300x600": {
+			"text": "THE NEW",
+			"size": 29,
+			"top": 84
+		},
+		"160x600": {
+			"text": "THE NEW",
+			"size": 29,
+			"top": 36
+		},
+		"320x480": {
+			"text": "THE NEW",
+			"size": 42,
+			"top": 14
+		},
+		"320x320": {
+			"text": "THE NEW",
+			"size": 28,
+			"top": 16
+		},
+		"300x250": {
+			"text": "THE NEW",
+			"size": 26,
+			"top": 15
+		},
+		"320x50": {
+			"text": "THE NEW",
+			"size": 12.5,
+			"top": 5
+		},
+		"320x100": {
+			"text": "THE NEW",
+			"size": 18.5,
+			"top": 4
+		},
+		"728x90": {
+			"text": "THE NEW",
+			"size": 24.5,
+			"top": 6
+		},
+		"800x250": {
+			"text": "THE NEW",
+			"size": 36,
+			"top": 10
+		},
+		"970x250": {
+			"text": "THE NEW",
+			"size": 36,
+			"top": 12
+		}
+	},
+	"text2b": {
+		"300x600": {
+			"text": "MINI John Cooper Works.",
+			"size": 15,
+			"top": 117
+		},
+		"160x600": {
+			"text": "MINI John Cooper Works.",
+			"size": 10,
+			"top": 70
+		},
+		"320x480": {
+			"text": "MINI John Cooper Works.",
+			"size": 15,
+			"top": 60
+		},
+		"320x320": {
+			"text": "MINI John Cooper Works.",
+			"size": 13.5,
+			"top": 49
+		},
+		"300x250": {
+			"text": "MINI John Cooper Works.",
+			"size": 12.5,
+			"top": 45
+		},
+		"320x50": {
+			"text": "MINI John Cooper Works.",
+			"size": 7.5,
+			"top": 18
+		},
+		"320x100": {
+			"text": "MINI John Cooper Works.",
+			"size": 8.5,
+			"top": 25
+		},
+		"728x90": {
+			"text": "MINI John Cooper Works.",
+			"size": 12.5,
+			"top": 33
+		},
+		"800x250": {
+			"text": "MINI John Cooper Works.​",
+			"size": 17,
+			"top": 50
+		},
+		"970x250": {
+			"text": "MINI John Cooper Works.",
+			"size": 17,
+			"top": 51
+		}
+	},
+	"text1c": {
+		"300x600": {
+			"text": "",
+			"size": 29,
+			"top": 143
+		},
+		"160x600": {
+			"text": "",
+			"size": 19,
+			"top": 94
+		},
+		"320x480": {
+			"text": "",
+			"size": 42,
+			"top": 96
+		},
+		"320x320": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x50": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x100": {
+			"text": "",
+			"size": 18.5,
+			"top": 51
+		},
+		"728x90": {
+			"text": "",
+			"size": 25,
+			"top": 54
+		},
+		"800x250": {
+			"text": "",
+			"size": 36,
+			"top": 86
+		},
+		"970x250": {
+			"text": "",
+			"size": 34,
+			"top": 81
+		}
+	},
+	"text1d": {
+		"300x600": {
+			"text": "",
+			"size": 5,
+			"top": 0
+		},
+		"160x600": {
+			"text": "",
+			"size": 5,
+			"top": 0
+		},
+		"320x480": {
+			"text": "",
+			"size": 5,
+			"top": 0
+		},
+		"320x320": {
+			"text": "",
+			"size": 5,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x50": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x100": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"728x90": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"800x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"cta1": {
+		"300x600": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 150,
+			"height": 23
+		},
+		"160x600": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 99,
+			"height": 20
+		},
+		"320x480": {
+			"text": "LEARN MORE",
+			"size": 13,
+			"top": 90,
+			"height": 23
+		},
+		"320x320": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 76,
+			"height": 25
+		},
+		"300x250": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 72,
+			"height": 23
+		},
+		"320x50": {
+			"text": "LEARN MORE",
+			"size": 7,
+			"top": 30,
+			"height": 12
+		},
+		"320x100": {
+			"text": "LEARN MORE",
+			"size": 10,
+			"top": 48,
+			"height": 17
+		},
+		"728x90": {
+			"text": "LEARN MORE",
+			"size": 12.5,
+			"top": 55,
+			"height": 22
+		},
+		"800x250": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 93,
+			"height": 24
+		},
+		"970x250": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 93,
+			"height": 24
+		}
+	},
+	"text1a": {
+		"300x600": {
+			"text": "FEEL THE",
+			"size": 27,
+			"top": 85
+		},
+		"160x600": {
+			"text": "FEEL THE",
+			"size": 20,
+			"top": 45
+		},
+		"320x480": {
+			"text": "FEEL THE",
+			"size": 27,
+			"top": 22
+		},
+		"320x320": {
+			"text": "FEEL THE",
+			"size": 23.5,
+			"top": 18
+		},
+		"300x250": {
+			"text": "FEEL THE",
+			"size": 22,
+			"top": 18
+		},
+		"320x50": {
+			"text": "FEEL THE",
+			"size": 9.5,
+			"top": 6
+		},
+		"320x100": {
+			"text": "FEEL THE",
+			"size": 14.5,
+			"top": 6
+		},
+		"728x90": {
+			"text": "FEEL THE",
+			"size": 17,
+			"top": 10
+		},
+		"800x250": {
+			"text": "FEEL THE",
+			"size": 30,
+			"top": 12
+		},
+		"970x250": {
+			"text": "FEEL THE",
+			"size": 30,
+			"top": 12
+		}
+	},
+	"text1b": {
+		"300x600": {
+			"text": "O-M-G FORCE.",
+			"size": 27,
+			"top": 113
+		},
+		"160x600": {
+			"text": "O-M-G FORCE.",
+			"size": 20,
+			"top": 65
+		},
+		"320x480": {
+			"text": "O-M-G FORCE.",
+			"size": 27,
+			"top": 49
+		},
+		"320x320": {
+			"text": "O-M-G FORCE.",
+			"size": 23.5,
+			"top": 41
+		},
+		"300x250": {
+			"text": "O-M-G FORCE.",
+			"size": 22,
+			"top": 40
+		},
+		"320x50": {
+			"text": "O-M-G FORCE.",
+			"size": 9.5,
+			"top": 15
+		},
+		"320x100": {
+			"text": "O-M-G FORCE.",
+			"size": 14.5,
+			"top": 20
+		},
+		"728x90": {
+			"text": "O-M-G FORCE.",
+			"size": 17,
+			"top": 28
+		},
+		"800x250": {
+			"text": "O-M-G FORCE.",
+			"size": 30,
+			"top": 46
+		},
+		"970x250": {
+			"text": "O-M-G FORCE.",
+			"size": 30,
+			"top": 47
+		}
+	},
+	"text2c": {
+		"300x600": {
+			"text": "",
+			"size": 27,
+			"top": 139
+		},
+		"160x600": {
+			"text": "",
+			"size": 13,
+			"top": 78
+		},
+		"320x480": {
+			"text": "",
+			"size": 27,
+			"top": 76
+		},
+		"320x320": {
+			"text": "",
+			"size": 23.5,
+			"top": 64
+		},
+		"300x250": {
+			"text": "",
+			"size": 22,
+			"top": 63
+		},
+		"320x50": {
+			"text": "",
+			"size": 26,
+			"top": 71
+		},
+		"320x100": {
+			"text": "",
+			"size": 14.5,
+			"top": 33
+		},
+		"728x90": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"800x250": {
+			"text": "",
+			"size": 36,
+			"top": 86
+		},
+		"970x250": {
+			"text": "",
+			"size": 34,
+			"top": 82
+		}
+	},
+	"text2d": {
+		"300x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"160x600": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x480": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x320": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"300x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x50": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"320x100": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"728x90": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"800x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		},
+		"970x250": {
+			"text": "",
+			"size": 0,
+			"top": 0
+		}
+	},
+	"cta2": {
+		"300x600": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 145,
+			"height": 23
+		},
+		"160x600": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 93,
+			"height": 20
+		},
+		"320x480": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 90,
+			"height": 23
+		},
+		"320x320": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 75,
+			"height": 25
+		},
+		"300x250": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 69,
+			"height": 23
+		},
+		"320x50": {
+			"text": "LEARN MORE",
+			"size": 7,
+			"top": 30,
+			"height": 12
+		},
+		"320x100": {
+			"text": "LEARN MORE",
+			"size": 10,
+			"top": 48,
+			"height": 17
+		},
+		"728x90": {
+			"text": "LEARN MORE",
+			"size": 12.5,
+			"top": 55,
+			"height": 22
+		},
+		"800x250": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 82,
+			"height": 23
+		},
+		"970x250": {
+			"text": "LEARN MORE",
+			"size": 14,
+			"top": 82,
+			"height": 23
+		}
+	}
+}
