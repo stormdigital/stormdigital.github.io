3c1c599269997c5424aebe098fa51c126a85ab6b
Dave Tervoort <dave.tervoort@accenture.com>
2024-11-04 15:47:01 +0100

    added font + no bg version

--- preview/Exact_Belgie/250x250_no_bg/main.js
+++ preview/Exact_Belgie/250x250_no_bg/main.js
@@ -34,9 +34,9 @@ function getAnimation(){
         gsap.set("#loaderWrapper", {y:9999999})
     }}, "start")
     mainTL.to("#person", 1.5, {scale:1.3, x:-15, y:-13, ease:Power3.easeInOut}, "+=0.5")
-    mainTL.to("#bg", 1.5, {scale:1.15, x:0, ease:Power3.easeInOut}, "-=1.5")
+    // mainTL.to("#bg", 1.5, {scale:1.15, x:0, ease:Power3.easeInOut}, "-=1.5")
     mainTL.to("#person", 5, {scale:1, x:65, y:0, ease:Sine.easeInOut})
-    mainTL.to("#bg", 5, {scale:1, x:50, ease:Sine.easeInOut}, "-=5")
+    // mainTL.to("#bg", 5, {scale:1, x:50, ease:Sine.easeInOut}, "-=5")
     mainTL.from(splitT1.words, 0.3, {opacity: 0, x:-20, ease: Sine.easeOut, stagger: 0.1}, "-=4.5");
     mainTL.from("#text2Wrapper", 0.5, {x:-200, ease:Power3.easeOut}, "-=2")
 
