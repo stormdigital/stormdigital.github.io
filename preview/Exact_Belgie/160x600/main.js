223d32f9c6aa1f653892329b346627e777c129fb
Dave Tervoort <dave.tervoort@accenture.com>
2024-11-05 13:52:57 +0100

    small feedback

--- preview/Exact_Belgie/160x600/main.js
+++ preview/Exact_Belgie/160x600/main.js
@@ -26,7 +26,7 @@ function init() {
 function getAnimation(){
     
     var splitT1 = new SplitText("#text1", {type: "words"});
-    var splitT2 = new SplitText("#text2", {type: "lines"});
+    var splitT2 = new SplitText("#text2", {type: "words"});
     
 
     mainTL = new TimelineLite({repeat:2});
@@ -40,7 +40,8 @@ function getAnimation(){
     mainTL.to("#bg", 2, {scale:1, ease:Sine.easeInOut}, "-=2")
     mainTL.from(splitT1.words, 0.3, {opacity: 0, x:-20, ease: Sine.easeOut, stagger: 0.1}, "-=1.5");
     mainTL.from("#text2Wrapper", 0.5, {x:-200, ease:Power3.easeOut}, "+=0.7")
-    mainTL.from(splitT2.lines, 0.3, {opacity: 0, x:-20, ease: Sine.easeOut, stagger: 0.6}, "-=0.1");
+    mainTL.from([splitT2.words[0], splitT2.words[1], splitT2.words[2], splitT2.words[3], splitT2.words[4]], 0.3, {opacity: 0, x:-20, ease: Sine.easeOut}, "-=0.1");
+    mainTL.from([splitT2.words[5], splitT2.words[6], splitT2.words[7], splitT2.words[8], splitT2.words[9]], 0.3, {opacity: 0, x:-20, ease: Sine.easeOut}, "+=1");
     mainTL.from("#cta", 0.5, {scale:0, ease:Back.easeOut}, "+=0.3");
     
     mainTL.to("#cta", 0.25, {scale:1.1, repeat:1, yoyo:true, ease:Sine.easeInOut}, "+=0.5");
