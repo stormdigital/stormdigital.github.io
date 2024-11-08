8e337bad64312388f789ae684def1fc6c5cce2f6
Dave Tervoort <dave.tervoort@accenture.com>
2024-11-08 12:06:30 +0100

    PLK Xmas KW47

--- preview/Aldi/Aldi_PLK_Xmas_2024/KW46/300x250_A/main.js
+++ preview/Aldi/Aldi_PLK_Xmas_2024/KW46/300x250_A/main.js
@@ -15,8 +15,7 @@ function init() {
     })
 
     document.querySelector("#mainExit").addEventListener("click", function(){
-        // masterTL.progress(1);
-        masterTL.pause();
+        masterTL.progress(1);
     })
 
     getAnimation();
