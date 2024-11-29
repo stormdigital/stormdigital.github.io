036ae8e1b1ae9e52a5048e60ff55037867800fe5
Dave Tervoort <dave.tervoort@accenture.com>
2024-11-29 16:45:35 +0100

    Travel master v2

--- preview/Aldi/Aldi_Travel/300x250_v2/main.js
+++ preview/Aldi/Aldi_Travel/300x250_v2/main.js
@@ -3,7 +3,8 @@ myFT.on("ready", init);
 function init() {
 
     document.querySelector("#mainExit").addEventListener("click", function(){
-        masterTL.progress(1).pause();
+        // masterTL.progress(1).pause();
+        masterTL.pause();
     })
 
     document.querySelector("#mainExit").addEventListener("mouseover", function(){
@@ -23,31 +24,30 @@ function init() {
 
 function getAnimation(){
 
+    gsap.set("#bluePanel", {height: 35});
+
     masterTL = gsap.timeline({paused:true, repeat:1});
 
     masterTL.to("#loaderWrapper", 0.2, {opacity:0, ease:Sine.easeInOut, onComplete:function(){
         gsap.set("#loaderWrapper", {y:999999})
     }}, 0)    
-    masterTL.from("#bg", 2, {scale:1.1, ease:Sine.easeOut}, 0)    
     masterTL.from("#travelLogo", 0.5, {x:-150, ease:Sine.easeOut}, 0.5)    
-    masterTL.from("#hand", 1.5, {x:-250, y:230, rotation:-25, ease: "back.out(1.2)"}, 1)    
+    // masterTL.from("#hand", 1.5, {x:-250, y:230, rotation:-25, ease: "back.out(1.2)"}, 1)    
     
-    masterTL.from("#text1A", 0.3, {opacity:0, y:20, x:-10, ease:Sine.easeOut})    
-    masterTL.from("#text1B", 0.3, {opacity:0, y:20, x:-10, ease:Sine.easeOut}, "+=0.3")    
-    masterTL.from("#text1C", 0.3, {opacity:0, y:20, x:-10, ease:Sine.easeOut}, "+=0.3")    
-    masterTL.from("#destWrapper", 1, {width:0, ease:Sine.easeOut}, "+=0.3")    
+    masterTL.from("#text1", 0.5, {opacity:0, ease:Sine.easeOut})
+    masterTL.from("#destination", 1, {y:200, ease:Power2.easeOut}, "+=0")    
     
     masterTL.from("#price", 0.5, {scale:0, ease:Back.easeOut}, "+=0")   
     
-    masterTL.to("#hand", 1, {x:-250, y:230, rotation:-25, ease: "back.in(1.1)"}, "+=2") 
-    masterTL.to(["#text1A", "#text1B", "#text1C", "#destWrapper"], 0.3, {opacity:0, ease:Sine.easeOut}, "-=1")   
-    masterTL.to("#price", 0.3, {scale:0, ease:Back.easeIn}, "-=1")  
+    masterTL.to(["#text1", "#destWrapper", "#hand", "#price"], 0.3, {opacity:0, ease:Sine.easeOut}, "+=2")
 
-    masterTL.from("#bluePanel", 0.5, {scale:0, ease:Back.easeOut}, "+=0")
-    masterTL.from("#usp1", 0.3, {opacity:0, x:-20, ease:Sine.easeOut}, "+=0.1")
-    masterTL.from("#usp2", 0.3, {opacity:0, x:-20, ease:Sine.easeOut}, "+=0.1")
-    masterTL.from("#usp3", 0.3, {opacity:0, x:-20, ease:Sine.easeOut}, "+=0.1")
-    masterTL.from("#usp4", 0.3, {opacity:0, x:-20, ease:Sine.easeOut}, "+=0.1")
+    masterTL.from(["#bluePanel", "#usp1"], 0.75, {scale:0, ease:Back.easeOut}, "+=0")
+    masterTL.to("#bluePanel", 0.3, {height:62, ease:Sine.easeInOut}, "+=0")
+    masterTL.from("#usp2", 0.5, {scale:0, ease:"back.out(1.2)"}, "+=0")
+    masterTL.to("#bluePanel", 0.3, {height:90, ease:Sine.easeInOut}, "+=0")
+    masterTL.from("#usp3", 0.5, {scale:0, ease:"back.out(1.2)"}, "+=0.1")
+    masterTL.to("#bluePanel", 0.3, {height:120, ease:Sine.easeInOut}, "+=0")
+    masterTL.from("#usp4", 0.5, {scale:0, ease:"back.out(1.2)"}, "+=0.1")
 
     masterTL.from("#ctaLeft", 0.5, {scale:0, ease:Back.easeOut}, "+=0.3")
     masterTL.from("#ctaRight", 0.00001, {opacity:0}, "+=0")
@@ -56,8 +56,5 @@ function getAnimation(){
     masterTL.to("#banner", 0.5, {}, "14.5")
 
     masterTL.play();
-
-    console.log(masterTL.duration());
-    
 
 }
