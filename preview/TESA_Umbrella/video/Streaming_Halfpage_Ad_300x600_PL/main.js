8413eef84191d5caac866b922f126ed089dd1d59
Dave Tervoort <dave.tervoort@accenture.com>
2024-12-05 10:24:09 +0100

    Tesa small animation fix

--- preview/TESA_Umbrella/video/Streaming_Halfpage_Ad_300x600_PL/main.js
+++ preview/TESA_Umbrella/video/Streaming_Halfpage_Ad_300x600_PL/main.js
@@ -44,17 +44,17 @@ function getAnimation(){
     masterTL.to("#cta", 0.25, {scale:1.15, ease:Sine.easeInOut, repeat:1, yoyo:true}, "start+=1")
     masterTL.to("#ctaBorder", 0.5, {scale:1.8, ease:Sine.easeIn, opacity:0}, "start+=1")
     masterTL.to("#ctaBorder", 0.001, {scale:1, opacity:1}, "start+=1.999")
-    masterTL.to("#text1", 0.4, {x:50, opacity:0, ease:Power2.easeInOut}, "start+=7.5")
-    masterTL.from("#text2", 0.7, {x:-50, opacity:0, ease:Power2.easeInOut}, "start+=8")
-    masterTL.from("#mainProduct", 0.5, {opacity:0, y:70, ease:"elastic.out(0.5 ,0.4)"}, "start+=8")
-    masterTL.from("#leftProduct", 0.4, {x:70, opacity:0, ease:Back.easeOut}, "start+=8.3")
-    masterTL.from("#rightProduct", 0.4, {x:-70, opacity:0, ease:Back.easeOut}, "start+=8.3")
+    masterTL.to("#text1", 0.4, {x:50, opacity:0, ease:Power2.easeInOut}, "start+=4.5")
+    masterTL.from("#text2", 0.7, {x:-50, opacity:0, ease:Power2.easeInOut}, "start+=5")
+    masterTL.from("#mainProduct", 0.5, {opacity:0, y:70, ease:"elastic.out(0.5 ,0.4)"}, "start+=5")
+    masterTL.from("#leftProduct", 0.4, {x:70, opacity:0, ease:Back.easeOut}, "start+=5.3")
+    masterTL.from("#rightProduct", 0.4, {x:-70, opacity:0, ease:Back.easeOut}, "start+=5.3")
     masterTL.to("#cta", 0.25, {scale:1.15, ease:Sine.easeInOut, repeat:1, yoyo:true}, "start+=10")
-    masterTL.to("#ctaBorder", 0.5, {scale:1.8, ease:Sine.easeIn, opacity:0}, "start+=10")
-    masterTL.to("#ctaBorder", 0.5, {opacity:0}, "start+=10.5")
+    masterTL.to("#ctaBorder", 0.5, {scale:1.8, ease:Sine.easeIn, opacity:0}, "start+=7")
+    masterTL.to("#ctaBorder", 0.5, {opacity:0}, "start+=7.5")
     masterTL.to("#ctaBorder", 0.5, {opacity:0, onComplete:function(){
         gsap.set("#replay", {display:"block"});
-    }}, "start+=10.5")
+    }}, "start+=7.5")
 
     if (document.querySelector("#video").readyState === 4 ) {
         masterTL.play();
