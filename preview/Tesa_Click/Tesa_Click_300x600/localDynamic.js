3d898cad6305e5d3ca7d0c17a409d2da2075a188
Dave Tervoort <dave.tervoort@accenture.com>
2025-02-18 16:41:09 +0100

    Tesa click master

---
+++ preview/Tesa_Click/Tesa_Click_300x600/localDynamic.js
@@ -0,0 +1,25 @@
+localDynamicData = {
+	"variationName": "GER_01",
+	"campaignName": "GER",
+	"dimensionName": {
+		"300x600": "Tesa_Click_300x600"
+	},
+	"text1": "./text1.png",
+	"highlight1": "./highlight1.png",
+	"text2": "./text2.png",
+	"highlight2": "./highlight2.png",
+	"text3": "./text3.png",
+	"highlight3": "./highlight3.png",
+	"text4": "./text4.png",
+	"highlight4": "./highlight4.png",
+	"text5": "./text5.png",
+	"logo": "./logo.svg",
+	"tadaa": "./tadaa.svg",
+	"bg1": "./bg1.jpg",
+	"bg2": "./bg2.jpg",
+	"bg3": "./bg3.jpg",
+	"bg4": "./bg4.jpg",
+	"product": "./product_middle.png",
+	"cta": "./cta.png",
+	"ctaBorder": "./cta_border.png"
+}
