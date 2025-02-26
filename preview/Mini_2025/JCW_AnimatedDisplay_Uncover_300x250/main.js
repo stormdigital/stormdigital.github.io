95261191b43bf9d3bd623d89643df0198da15d64
Fatih <fatih.yalcin@accenture.com>
2025-02-26 11:26:47 +0100

    mini uncover firs sizes added

--- preview/Mini_2025/JCW_AnimatedDisplay_Uncover_300x250/main.js
+++ preview/Mini_2025/JCW_AnimatedDisplay_Uncover_300x250/main.js
@@ -32,21 +32,21 @@ function getAnimation(){
     masterTL = new TimelineLite({paused:true, repeat:1});
     masterTL.add("start");
 
-    masterTL.from("#bg1", 4, {scale:1.03, transformOrigin:"80% 100%", ease:Sine.easeInOut}, "start")
+    masterTL.from("#bg1", 4, {scale:1.1, transformOrigin:"80% 100%", ease:Sine.easeInOut}, "start")
     masterTL.from("#text1A", 0.5, {x:-20, opacity: 0, ease: Sine.easeOut}, "start+=0.5");
     masterTL.from("#text1B", 0.5, {x:-20, opacity: 0, ease: Sine.easeOut}, "start+=0.75");
-    // masterTL.from("#text1C", 0.5, {x:-20, opacity: 0, ease: Sine.easeOut}, "start+=0.9");
+    masterTL.from("#text1C", 0.5, {x:-20, opacity: 0, ease: Sine.easeOut}, "start+=0.9");
     masterTL.to("#text1", 0.3, {opacity:0, ease:Sine.easeIn})
 
     masterTL.add("switchBg1", "-=0.3")
-    masterTL.to("#bg1", 0.3, {scale:1.03, ease:Sine.easeInOut}, "switchBg1")
+    masterTL.to("#bg1", 0.3, {scale:1.1, ease:Sine.easeInOut}, "switchBg1")
     masterTL.from("#bg2", 0.3, {scale:1.1, opacity:0, ease:Sine.easeInOut}, "switchBg1")
     masterTL.to("#bg1", 0.15, {opacity:0, ease:Sine.easeInOut}, "switchBg1+=0.15")
 
     masterTL.from("#text2A", 0.5, {x:20, opacity: 0, ease: Sine.easeOut}, "switchBg1+=0.5");
     masterTL.from("#text2B", 0.5, {x:20, opacity: 0, ease: Sine.easeOut}, "switchBg1+=0.75");
     // masterTL.from("#text2C", 0.5, {x:20, opacity: 0, ease: Sine.easeOut}, "switchBg1+=0.9");
-    masterTL.from("#cta", 0.5, {scale:0.8, ease: Back.easeOut}, "switchBg1+=1.3");
+    masterTL.from("#cta", 0.5, {scale:0, ease: Back.easeOut}, "switchBg1+=1.3");
     
     for (let i = 3; i <= 12; i++) {
         console.log(i);
